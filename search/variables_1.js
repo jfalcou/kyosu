55b26dbce828c054a37dc271b65ef5100d3e8219
jfalcou <jfalcou@users.noreply.github.com>
2023-09-06 09:13:27 +0000

    deploy: cc1b64c7914aebdc5e70db2623f54f62ad5b56a5

--- search/variables_1.js
+++ search/variables_1.js
@@ -5,8 +5,11 @@ var searchData=
   ['convert_2',['convert',['../group__functions_gae8e08be7b28ad1cf9159ae564115867f.html#gae8e08be7b28ad1cf9159ae564115867f',1,'kyosu']]],
   ['cos_3',['cos',['../group__functions_ga788b51ca9d3366567828c74d46f12168.html#ga788b51ca9d3366567828c74d46f12168',1,'kyosu']]],
   ['cosh_4',['cosh',['../group__functions_gaa608876852fffef8d90a4f7280ff4549.html#gaa608876852fffef8d90a4f7280ff4549',1,'kyosu']]],
-  ['cot_5',['cot',['../group__functions_ga783725b137881f9e6e60e1f9b3bae9fc.html#ga783725b137881f9e6e60e1f9b3bae9fc',1,'kyosu']]],
-  ['coth_6',['coth',['../group__functions_gaa651f9b616ad1022127142bf9cdc68e8.html#gaa651f9b616ad1022127142bf9cdc68e8',1,'kyosu']]],
-  ['csc_7',['csc',['../group__functions_gaecbb072c068e6effd78aa84c6e65b329.html#gaecbb072c068e6effd78aa84c6e65b329',1,'kyosu']]],
-  ['csch_8',['csch',['../group__functions_ga6061b321a4cb255fbbcae1992d42aafa.html#ga6061b321a4cb255fbbcae1992d42aafa',1,'kyosu']]]
+  ['cospi_5',['cospi',['../group__functions_gaa683fad544774c6d87efc39b424f78e0.html#gaa683fad544774c6d87efc39b424f78e0',1,'kyosu']]],
+  ['cot_6',['cot',['../group__functions_ga783725b137881f9e6e60e1f9b3bae9fc.html#ga783725b137881f9e6e60e1f9b3bae9fc',1,'kyosu']]],
+  ['coth_7',['coth',['../group__functions_gaa651f9b616ad1022127142bf9cdc68e8.html#gaa651f9b616ad1022127142bf9cdc68e8',1,'kyosu']]],
+  ['cotpi_8',['cotpi',['../group__functions_ga0d165215c9a5f3f78feab4b70874f436.html#ga0d165215c9a5f3f78feab4b70874f436',1,'kyosu']]],
+  ['csc_9',['csc',['../group__functions_gaecbb072c068e6effd78aa84c6e65b329.html#gaecbb072c068e6effd78aa84c6e65b329',1,'kyosu']]],
+  ['csch_10',['csch',['../group__functions_ga6061b321a4cb255fbbcae1992d42aafa.html#ga6061b321a4cb255fbbcae1992d42aafa',1,'kyosu']]],
+  ['cscpi_11',['cscpi',['../group__functions_gaae7085c537556dd281121a349ffb2b93.html#gaae7085c537556dd281121a349ffb2b93',1,'kyosu']]]
 ];
--- search/variables_f.js
+++ search/variables_f.js
@@ -2,12 +2,15 @@ var searchData=
 [
   ['sec_0',['sec',['../group__functions_ga308a170dbf61d3a4aba52d975abf902e.html#ga308a170dbf61d3a4aba52d975abf902e',1,'kyosu']]],
   ['sech_1',['sech',['../group__functions_ga9b2e4c2e043be340f42910dbf5ec54ed.html#ga9b2e4c2e043be340f42910dbf5ec54ed',1,'kyosu']]],
-  ['sign_2',['sign',['../group__functions_ga961299cde75efe464ea2e3f9944d1f33.html#ga961299cde75efe464ea2e3f9944d1f33',1,'kyosu']]],
-  ['sin_3',['sin',['../group__functions_ga26c8821ba25972647692322f5236a370.html#ga26c8821ba25972647692322f5236a370',1,'kyosu']]],
-  ['sincos_4',['sincos',['../group__functions_gaf34684f4a02ca7468c2f7cb440779a6d.html#gaf34684f4a02ca7468c2f7cb440779a6d',1,'kyosu']]],
-  ['sinh_5',['sinh',['../group__functions_ga1e253b4aba99259dbb98d891247c2a38.html#ga1e253b4aba99259dbb98d891247c2a38',1,'kyosu']]],
-  ['sinhcosh_6',['sinhcosh',['../group__functions_ga3aa2bb7847bd2885618e263ed0f0cd82.html#ga3aa2bb7847bd2885618e263ed0f0cd82',1,'kyosu']]],
-  ['sqr_7',['sqr',['../group__functions_ga6ead34ff89aeb18c2380188af5f83dcd.html#ga6ead34ff89aeb18c2380188af5f83dcd',1,'kyosu']]],
-  ['sqr_5fabs_8',['sqr_abs',['../group__functions_ga297cbe86ab12760ec934f76879f805d0.html#ga297cbe86ab12760ec934f76879f805d0',1,'kyosu']]],
-  ['sqrt_9',['sqrt',['../group__functions_ga6f78a4312ac7bd38e25d0b09b18fe3e6.html#ga6f78a4312ac7bd38e25d0b09b18fe3e6',1,'kyosu']]]
+  ['secpi_2',['secpi',['../group__functions_ga606d275eaf727424c499a9ddb6a09cd6.html#ga606d275eaf727424c499a9ddb6a09cd6',1,'kyosu']]],
+  ['sign_3',['sign',['../group__functions_ga961299cde75efe464ea2e3f9944d1f33.html#ga961299cde75efe464ea2e3f9944d1f33',1,'kyosu']]],
+  ['sin_4',['sin',['../group__functions_ga26c8821ba25972647692322f5236a370.html#ga26c8821ba25972647692322f5236a370',1,'kyosu']]],
+  ['sincos_5',['sincos',['../group__functions_gaf34684f4a02ca7468c2f7cb440779a6d.html#gaf34684f4a02ca7468c2f7cb440779a6d',1,'kyosu']]],
+  ['sinh_6',['sinh',['../group__functions_ga1e253b4aba99259dbb98d891247c2a38.html#ga1e253b4aba99259dbb98d891247c2a38',1,'kyosu']]],
+  ['sinhcosh_7',['sinhcosh',['../group__functions_ga3aa2bb7847bd2885618e263ed0f0cd82.html#ga3aa2bb7847bd2885618e263ed0f0cd82',1,'kyosu']]],
+  ['sinpi_8',['sinpi',['../group__functions_gacb9f75de7a67c7d512f135c26b10cdf1.html#gacb9f75de7a67c7d512f135c26b10cdf1',1,'kyosu']]],
+  ['sinpicospi_9',['sinpicospi',['../group__functions_gabfd2527bde97bcd86c6d1933fce660a1.html#gabfd2527bde97bcd86c6d1933fce660a1',1,'kyosu']]],
+  ['sqr_10',['sqr',['../group__functions_ga6ead34ff89aeb18c2380188af5f83dcd.html#ga6ead34ff89aeb18c2380188af5f83dcd',1,'kyosu']]],
+  ['sqr_5fabs_11',['sqr_abs',['../group__functions_ga297cbe86ab12760ec934f76879f805d0.html#ga297cbe86ab12760ec934f76879f805d0',1,'kyosu']]],
+  ['sqrt_12',['sqrt',['../group__functions_ga6f78a4312ac7bd38e25d0b09b18fe3e6.html#ga6f78a4312ac7bd38e25d0b09b18fe3e6',1,'kyosu']]]
 ];
