e0f3e089283125ff7f9d988d4a1d610dc28b8d0d
jtlap <jtlapreste@gmail.com>
2023-09-11 17:50:17 +0200

    wrong macosx test for atanh

--- test/unit/complex/atanh.cpp
+++ test/unit/complex/atanh.cpp
@@ -56,39 +56,44 @@ auto sc( auto  ec)
 TTS_CASE_TPL( "Check corner casesof eve::atanh", kyosu::scalar_real_types)
   <typename T>(tts::type<T>)
 {
-  using e_t = T;
-  auto tcx = kyosu::to_complex;
-  using c_t = decltype(tcx(e_t(0)));
-  using eve::as;
-  const int N = 16;
-  std::array<c_t, N> inputs =
-    { c_t(eve::zero(as<e_t>()),eve::zero(as<e_t>())),//0
-      c_t(eve::inf(as<e_t>()),eve::zero(as<e_t>())), //1
-      c_t(eve::minf(as<e_t>()),eve::zero(as<e_t>())),//2
-      c_t(eve::nan(as<e_t>()),eve::zero(as<e_t>())), //3
-      c_t(eve::zero(as<e_t>()),eve::inf(as<e_t>())), //4
-      c_t(eve::inf(as<e_t>()),eve::inf(as<e_t>())),  //5
-      c_t(eve::minf(as<e_t>()),eve::inf(as<e_t>())), //6
-      c_t(eve::nan(as<e_t>()),eve::inf(as<e_t>())),  //7
-      c_t(eve::zero(as<e_t>()),eve::minf(as<e_t>())),//8--
-      c_t(eve::inf(as<e_t>()),eve::minf(as<e_t>())), //9
-      c_t(eve::minf(as<e_t>()),eve::minf(as<e_t>())),//10
-      c_t(eve::nan(as<e_t>()),eve::minf(as<e_t>())), //11
-      c_t(eve::zero(as<e_t>()),eve::nan(as<e_t>())), //12
-      c_t(eve::inf(as<e_t>()),eve::nan(as<e_t>())),  //13
-      c_t(eve::minf(as<e_t>()),eve::nan(as<e_t>())), //14
-      c_t(eve::nan(as<e_t>()),eve::nan(as<e_t>())),  //15
-    };
-
-  for(int i=0; i < N; ++i)
+  if constexpr(spy::stdlib != spy::libcpp_)
   {
-    if((i != 2 && i != 1)){
-      // this curious test corresponds to the fact that neither std::atanh nor std::atanh are correct for inputs (0, inf) or (0, -inf)
-      // peculiarly they contredict the C99 specification that atanh is odd
-      // atanh should behave "the same as C99 function catanh, defined in subclause 7.3.6.3 and G.5.2.3."
-      // the if clause has to be removed if/when libc++ will be corrected
-      TTS_IEEE_EQUAL(kyosu::atanh(inputs[i]), cv(std::atanh(sc(inputs[i]))));
-      TTS_IEEE_EQUAL(kyosu::atanh(-inputs[i]), cv(std::atanh(sc(-inputs[i]))));
+    using e_t = T;
+    auto tcx = kyosu::to_complex;
+    using c_t = decltype(tcx(e_t(0)));
+    using eve::as;
+    const int N = 16;
+    std::array<c_t, N> inputs =
+      { c_t(eve::zero(as<e_t>()),eve::zero(as<e_t>())),//0
+        c_t(eve::inf(as<e_t>()),eve::zero(as<e_t>())), //1
+        c_t(eve::minf(as<e_t>()),eve::zero(as<e_t>())),//2
+        c_t(eve::nan(as<e_t>()),eve::zero(as<e_t>())), //3
+        c_t(eve::zero(as<e_t>()),eve::inf(as<e_t>())), //4
+        c_t(eve::inf(as<e_t>()),eve::inf(as<e_t>())),  //5
+        c_t(eve::minf(as<e_t>()),eve::inf(as<e_t>())), //6
+        c_t(eve::nan(as<e_t>()),eve::inf(as<e_t>())),  //7
+        c_t(eve::zero(as<e_t>()),eve::minf(as<e_t>())),//8--
+        c_t(eve::inf(as<e_t>()),eve::minf(as<e_t>())), //9
+        c_t(eve::minf(as<e_t>()),eve::minf(as<e_t>())),//10
+        c_t(eve::nan(as<e_t>()),eve::minf(as<e_t>())), //11
+        c_t(eve::zero(as<e_t>()),eve::nan(as<e_t>())), //12
+        c_t(eve::inf(as<e_t>()),eve::nan(as<e_t>())),  //13
+        c_t(eve::minf(as<e_t>()),eve::nan(as<e_t>())), //14
+        c_t(eve::nan(as<e_t>()),eve::nan(as<e_t>())),  //15
+      };
+
+    for(int i=0; i < N; ++i)
+    {
+      if((i != 2 && i != 1)){
+        // this curious test corresponds to the fact that neither std::atanh nor std::atanh are correct for inputs (0, inf) or (0, -inf)
+        // peculiarly they contredict the C99 specification that atanh is odd
+        // atanh should behave "the same as C99 function catanh, defined in subclause 7.3.6.3 and G.5.2.3."
+        // the if clause has to be removed if/when libc++ will be corrected
+        TTS_IEEE_EQUAL(kyosu::atanh(inputs[i]), cv(std::atanh(sc(inputs[i]))));
+        TTS_IEEE_EQUAL(kyosu::atanh(-inputs[i]), cv(std::atanh(sc(-inputs[i]))));
+      }
     }
   }
+  else
+    TTS_PASS("no test for clang standard lib atanh corner cases are almost all false according C99");
 };
