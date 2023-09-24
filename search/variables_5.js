5444ca8e51a04a1f87206951af1c91f190e0b6d7
jfalcou <jfalcou@users.noreply.github.com>
2023-09-24 15:08:39 +0000

    deploy: 65412d33d9bec22ee5f9eaa06e4b9d46de302950

--- search/variables_5.js
+++ search/variables_5.js
@@ -5,11 +5,11 @@ var searchData=
   ['frac_2',['frac',['../group__functions_ga4c6ee3532d93a937aaa603ba4699b3ed.html#ga4c6ee3532d93a937aaa603ba4699b3ed',1,'kyosu']]],
   ['from_5fangle_5faxis_3',['from_angle_axis',['../group__quaternion_gaeb24f578562330d825ff8f68da4b9252.html#gaeb24f578562330d825ff8f68da4b9252',1,'kyosu']]],
   ['from_5fcylindrical_4',['from_cylindrical',['../group__quaternion_ga06868e959a899a9f2348b07c330c0048.html#ga06868e959a899a9f2348b07c330c0048',1,'kyosu']]],
-  ['from_5fcylindrospherical_5',['from_cylindrospherical',['../group__quaternion_ga36f4e319eab743013ddf5a868679fb63.html#ga36f4e319eab743013ddf5a868679fb63',1,'kyosu']]],
+  ['from_5fcylindrospherical_5',['from_cylindrospherical',['../group__quaternion_gaa17a7e084e8ef0ba4c64b55e456b1f57.html#gaa17a7e084e8ef0ba4c64b55e456b1f57',1,'kyosu']]],
   ['from_5feuler_6',['from_euler',['../group__quaternion_ga3f3485eda1a78b4bb970df61c7b602d7.html#ga3f3485eda1a78b4bb970df61c7b602d7',1,'kyosu']]],
-  ['from_5fmultipolar_7',['from_multipolar',['../group__quaternion_gadcb9029f90743da06cc952f8fe8120f0.html#gadcb9029f90743da06cc952f8fe8120f0',1,'kyosu']]],
+  ['from_5fmultipolar_7',['from_multipolar',['../group__quaternion_ga768dc300e06ddd80f84746662db3c01b.html#ga768dc300e06ddd80f84746662db3c01b',1,'kyosu']]],
   ['from_5fpolar_8',['from_polar',['../group__complex_gad70670d8278d34dc0f0faf883a48eab8.html#gad70670d8278d34dc0f0faf883a48eab8',1,'kyosu']]],
   ['from_5frotation_5fmatrix_9',['from_rotation_matrix',['../group__quaternion_gafe4deac1b486b7f1203090424e8df302.html#gafe4deac1b486b7f1203090424e8df302',1,'kyosu']]],
-  ['from_5fsemipolar_10',['from_semipolar',['../group__quaternion_ga6bd1f5ec343e8cf12fec6cc72eae2a87.html#ga6bd1f5ec343e8cf12fec6cc72eae2a87',1,'kyosu']]],
+  ['from_5fsemipolar_10',['from_semipolar',['../group__quaternion_gae4c3253d49e644eeea3531e3b588fc9c.html#gae4c3253d49e644eeea3531e3b588fc9c',1,'kyosu']]],
   ['from_5fspherical_11',['from_spherical',['../group__quaternion_ga943f811be2981809d36a4f31f52b0025.html#ga943f811be2981809d36a4f31f52b0025',1,'kyosu']]]
 ];
