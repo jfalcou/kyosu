3537c06d967304502bc8d92a5655e53721e83487
jtlap <jtlapreste@gmail.com>
2023-09-25 17:31:52 +0200

    asinh

--- test/unit/function/asinh.cpp
+++ test/unit/function/asinh.cpp
@@ -26,7 +26,7 @@ TTS_CASE_WITH ( "Check kyosu::asinh over quaternion"
   auto lr = kyosu::asinh(r);
   auto lc = kyosu::asinh(c);
   auto lq = kyosu::asinh(q);
-  TTS_RELATIVE_EQUAL(kyosu::sinh(lr), r, 1e-5);
+  TTS_RELATIVE_EQUAL(kyosu::sinh(lr), r, 1e-3);
   TTS_RELATIVE_EQUAL(kyosu::sinh(lc), c, 1e-3);
   TTS_RELATIVE_EQUAL(kyosu::sinh(lq), q, 1e-3);
 };
