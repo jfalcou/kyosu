73a215a4eb96a1eb16dabab10b8ef79dfff7920d
jtlap <jtlapreste@gmail.com>
2023-09-26 11:31:45 +0200

    unit test for to/from_polar

--- test/unit/function/to_polar.cpp
+++ test/unit/function/to_polar.cpp
@@ -12,10 +12,18 @@ TTS_CASE_WITH ( "Check behavior of from_polar on wide"
               , kyosu::real_types
               , tts::generate( tts::randoms(-1.0, +1.0)
                              , tts::randoms(-1.0, +1.0)
+                             , tts::randoms(-1.0, +1.0)
+                             , tts::randoms(-1.0, +1.0)
                              )
               )
-  <typename T>(T const& a0, T const& a1)
+  <typename T>(T const& a0, T const& a1, T const& a2, T const& a3)
 {
+  {
+    auto  q = kyosu::quaternion(a0, a1, a2, a3);
+    auto [r, t, iz] = kyosu::to_polar(q);
+    auto q1 = kyosu::from_polar(r, t, iz);
+    TTS_RELATIVE_EQUAL(q, q1, 1.0e-5);
+  }
   {
     auto  c = kyosu::complex(a0, a1);
     auto [r1, t1] = kyosu::to_polar(c);
