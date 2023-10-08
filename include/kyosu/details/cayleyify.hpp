f539592a7e0bba510d510516892ea0f4194b2467
jtlap <jtlapreste@gmail.com>
2023-10-08 22:40:34 +0200

    math use cayley_extend

--- include/kyosu/details/cayleyify.hpp
+++ include/kyosu/details/cayleyify.hpp
@@ -15,6 +15,14 @@ auto cayley_extend(auto f, auto z, auto... rs)
   return kyosu::real(c) + kyosu::ipart(c)*kyosu::sign(p);
 }
 
+auto cayley_extend_rev(auto f, auto z1, auto z2)
+{
+  auto p = kyosu::pure(z2);
+  auto az2 = kyosu::abs(p);
+  auto c = f(z1, kyosu::complex(kyosu::real(z2), az2));
+  return kyosu::real(c) + kyosu::ipart(c)*kyosu::sign(p);
+}
+
 auto cayley_extend2(auto f, auto z, auto... rs)
 {
   auto p = kyosu::pure(z);
