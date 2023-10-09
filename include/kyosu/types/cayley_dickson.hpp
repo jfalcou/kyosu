912f094cd8b15f4b3c762d70a2bf77a20a681e00
jtlap <jtlapreste@gmail.com>
2023-10-09 08:42:47 +0200

    no more specific complex functions

--- include/kyosu/types/cayley_dickson.hpp
+++ include/kyosu/types/cayley_dickson.hpp
@@ -17,7 +17,7 @@
 #include <kyosu/types/impl/trigo.hpp>
 #include <kyosu/types/impl/trigo_pi.hpp>
 #include <kyosu/types/impl/invtrig.hpp>
-#include <kyosu/types/impl/complex/special.hpp>
+#include <kyosu/types/impl/special.hpp>
 #include <kyosu/types/impl/quaternion/specific.hpp>
 
 #include <bit>
