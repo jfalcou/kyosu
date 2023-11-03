3459dce7a6539572dc4d29f95dc54e25f645a415
jtlap <jtlapreste@gmail.com>
2023-11-03 12:42:32 +0100

    spurious comments

--- include/kyosu/types/impl/detail/bessely1.hpp
+++ include/kyosu/types/impl/detail/bessely1.hpp
@@ -21,10 +21,7 @@ namespace kyosu::_
     using u_t   =  eve::underlying_type_t<Z>;
     auto twoopi = eve::two_o_pi(eve::as<u_t>());
     auto r1 = R(1, z);
-//    std::cout << "r1 " << r1 << std::endl;
     auto y0 = cyl_bessel_y0(z);
-//     std::cout << "y0 " << y0 << std::endl;
-//     std::cout << "j0 " << cyl_bessel_j0(z) << std::endl;
     auto recs1 = rec(r1)-twoopi/(z*cyl_bessel_j0(z)*y0);
     return if_else(is_eqz(z), complex(eve::minf(eve::as<u_t>())), y0*recs1);
   }
