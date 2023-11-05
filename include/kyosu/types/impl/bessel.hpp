123e25b5a1894bc8c9673644650cf83e2e4a01e6
jtlap <jtlapreste@gmail.com>
2023-11-05 14:09:29 +0100

    bessel_i

--- include/kyosu/types/impl/bessel.hpp
+++ include/kyosu/types/impl/bessel.hpp
@@ -15,9 +15,7 @@
 #include <kyosu/types/impl/detail/bessel_utils.hpp>
 
 #include <kyosu/types/impl/detail/bessel_j.hpp>
-#include <kyosu/types/impl/detail/besseli0.hpp>
-#include <kyosu/types/impl/detail/besseli1.hpp>
-#include <kyosu/types/impl/detail/besselin.hpp>
+#include <kyosu/types/impl/detail/bessel_i.hpp>
 #include <kyosu/types/impl/detail/bessely0.hpp>
 #include <kyosu/types/impl/detail/bessely1.hpp>
 #include <kyosu/types/impl/detail/besselyn.hpp>
