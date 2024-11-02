f0238b885cbc8b218b77a60bd71abb2c18759a3c
jtlap <jtlapreste@gmail.com>
2024-11-02 17:33:35 +0100

    abs.cpp

--- test/unit/function/abs.cpp
+++ test/unit/function/abs.cpp
@@ -23,7 +23,7 @@ TTS_CASE_WITH ( "Check kyosu::abs over complex"
               )
 (auto r, auto i)
 {
-  TTS_ULP_EQUAL(kyosu::abs(kyosu::complex(r,i)), eve::hypot(r, i), 0.5);
+  TTS_ULP_EQUAL(kyosu::abs(kyosu::complex(r,i)), eve::hypot[eve::pedantic](r, i), 0.5);
 };
 
 TTS_CASE_WITH ( "Check kyosu::abs over quaternion"
@@ -34,10 +34,10 @@ TTS_CASE_WITH ( "Check kyosu::abs over quaternion"
               )
 (auto r, auto i, auto j, auto k)
 {
-  TTS_ULP_EQUAL(kyosu::abs(kyosu::quaternion(r,i,j,k)), eve::hypot(r, i, j, k), 0.5);
+  TTS_ULP_EQUAL(kyosu::abs(kyosu::quaternion(r,i,j,k)), eve::hypot[eve::pedantic](r, i, j, k), 0.5);
 };
 
-TTS_CASE_WITH ( "Check kyosu::abs over quaternion"
+TTS_CASE_WITH ( "Check kyosu::abs over octonion"
               , kyosu::real_types
               , tts::generate ( tts::randoms(-10,10), tts::randoms(-10,10)
                               , tts::randoms(-10,10), tts::randoms(-10,10)
@@ -47,5 +47,5 @@ TTS_CASE_WITH ( "Check kyosu::abs over quaternion"
               )
 <typename T>(T r, T i, T j, T k, T l, T li, T lj, T lk)
 {
-  TTS_ULP_EQUAL(kyosu::abs(kyosu::octonion_t<T>(r,i,j,k,l,li,lj,lk)), eve::hypot(r, i, j, k, l, li, lj, lk), 0.5);
+  TTS_ULP_EQUAL(kyosu::abs(kyosu::octonion_t<T>(r,i,j,k,l,li,lj,lk)), eve::hypot[eve::pedantic](r, i, j, k, l, li, lj, lk), 0.5);
 };
