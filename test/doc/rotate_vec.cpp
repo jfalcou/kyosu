be003c8cdc7b61764ae11c49398454fdb6924e58
jtlap <jtlapreste@gmail.com>
2024-11-22 08:25:09 +0100

    spurious normalize

--- test/doc/rotate_vec.cpp
+++ test/doc/rotate_vec.cpp
@@ -9,7 +9,7 @@ int main()
   auto theta = eve::pio_3(eve::as<double>());
   std::array<double, 3> ax{1., 2., 3.};
   std::array<double, 3> v{ 1.5, 2.0, -3.0};
-  auto q = kyosu::from_angle_axis(theta, std::span(ax), kyosu::normalize);
+  auto q = kyosu::from_angle_axis(theta, std::span(ax));
   std::cout << " v                " << v[0] << ", " << v[1] << ",  " <<  v[2] << ")\n";
   std::cout << " q                " << q << std::endl;
   auto rv = kyosu::rotate_vec(q, std::span<double, 3>(v));
