f86f6c814f539689d0f22ec08800507bb9978e88
jtlap <jtlapreste@gmail.com>
2024-11-26 13:34:42 +0100

    sph*i2n

--- include/kyosu/functions/sph_bessel_i2n.hpp
+++ include/kyosu/functions/sph_bessel_i2n.hpp
@@ -43,7 +43,7 @@ namespace kyosu
 
     template<eve::integral_scalar_value V0, concepts::real V1>
     KYOSU_FORCEINLINE constexpr auto operator()(V0 v0, V1 v1) const noexcept
-    { return KYOSU_CALL(v0,v1); }
+    { return (*this)(v0,complex(v1)); }
 
     KYOSU_CALLABLE_OBJECT(sph_bessel_i2n_t, sph_bessel_i2n_);
 };
