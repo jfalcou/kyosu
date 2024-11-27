d8c459a535802c4c1ebe99f18e32458797ac3bda
jtlap <jtlapreste@gmail.com>
2024-11-27 14:31:05 +0100

    chi + test not working

---
+++ test/unit/function/chi.cpp
@@ -0,0 +1,30 @@
+//======================================================================================================================
+/*
+  Kyosu - Complex Without Complexes
+  Copyright : KYOSU Contributors & Maintainers
+  SPDX-License-Identifier: BSL-1.0
+*/
+//======================================================================================================================
+#include <kyosu/kyosu.hpp>
+#include <test.hpp>
+
+TTS_CASE_WITH ( "Check kyosu::chi over real"
+              , kyosu::real_types
+              , tts::generate(tts::randoms(-10,10))
+              )
+<typename T>(T data)
+{
+  auto b =  [](auto z){return kyosu::abs(z) < 5;};
+  TTS_RELATIVE_EQUAL(kyosu::chi(data, b), eve::chi(data, b), tts::prec<T>());
+};
+
+TTS_CASE_WITH ( "Check kyosu::chi over real"
+              , kyosu::real_types
+              , tts::generate(tts::randoms(-10,10), tts::randoms(-10,10))
+              )
+<typename T>(T a0, T a1)
+{
+  auto b =  [](auto z){return kyosu::abs(z) < 5;};
+  kyosu::complex_t<T> data(a0, a1);
+  TTS_RELATIVE_EQUAL(kyosu::chi(data, b), eve::chi(kyosu::abs(data), b), tts::prec<T>());
+};
