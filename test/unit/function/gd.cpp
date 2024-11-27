736351e9473a410a204fc1a9e518f80f2d2504a0
jtlap <jtlapreste@gmail.com>
2024-11-27 14:33:03 +0100

    gd

--- test/unit/function/gd.cpp
+++ test/unit/function/gd.cpp
@@ -24,5 +24,5 @@ TTS_CASE_WITH ( "Check kyosu::gd over real"
 <typename T>(T a0, T a1)
 {
   kyosu::complex_t<T> data(a0, a1);
-  TTS_RELATIVE_EQUAL(kyosu::tanh(kyosu::gd(data/2)), kyosu::tan(kyosu::gd(data)/2), tts::prec<T>(1.0e-2, 3.0e-4));
+  TTS_RELATIVE_EQUAL(kyosu::tanh(kyosu::gd(data/2)), kyosu::tan(kyosu::gd(data)/2), tts::prec<T>(1.0e-1, 3.0e-3));
 };
