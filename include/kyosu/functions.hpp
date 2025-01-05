43e03d9a6abc43196530e574571358f9f81bc2de
jtlap <jtlapreste@gmail.com>
2025-01-05 09:29:43 +0100

    elliptic_fe

--- include/kyosu/functions.hpp
+++ include/kyosu/functions.hpp
@@ -73,6 +73,7 @@
 #include <kyosu/functions/digamma.hpp>
 #include <kyosu/functions/dist.hpp>
 #include <kyosu/functions/dot.hpp>
+#include <kyosu/functions/elliptic_fe.hpp>
 #include <kyosu/functions/erf.hpp>
 #include <kyosu/functions/erfcx.hpp>
 #include <kyosu/functions/erfi.hpp>
