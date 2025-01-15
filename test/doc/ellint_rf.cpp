c7a117b4492322f42dc730509d2ed94b83d17c2a
jtlap <jtlapreste@gmail.com>
2025-01-15 08:51:50 +0100

    doc rf

--- test/doc/ellint_rf.cpp
+++ test/doc/ellint_rf.cpp
@@ -10,13 +10,13 @@ int main()
   auto rf=  eve::ellint_rf(1.0, 2.0, 0.0);
   std::cout<< " kyosu::ellint_rf(kyosu::complex(1.0), kyosu::complex(2.0), kyosu::complex(0.0));" << r << std::endl;
   std::cout<< " eve::ellint_rf(1.0, 2.0, 0.0)                                                   " << rf<< std::endl;
-   r =  kyosu::ellint_rf[eve::raw](kyosu::complex(0.0, 1.0), kyosu::complex(0.0, -1.0), kyosu::complex(0.0));
+   r =  kyosu::ellint_rf(kyosu::complex(0.0, 1.0), kyosu::complex(0.0, -1.0), kyosu::complex(0.0));
    rf=  eve::ellint_rf(0.5     , 1.0      , 0.0);
-  auto rr = kyosu::ellint_rf[eve::raw](kyosu::complex(0.5), kyosu::complex(1.0), kyosu::complex(0.0));
+  auto rr = kyosu::ellint_rf(kyosu::complex(0.5), kyosu::complex(1.0), kyosu::complex(0.0));
   std::cout<< " r " << r << std::endl;
   std::cout<< " rr "<< rr << std::endl;
   std::cout<< " rf " << rf << std::endl;
-  r =  kyosu::ellint_rf[eve::raw](kyosu::complex(-1.0, 1.0), kyosu::complex(0.0, 1.0), kyosu::complex(0.0));
+  r =  kyosu::ellint_rf(kyosu::complex(-1.0, 1.0), kyosu::complex(0.0, 1.0), kyosu::complex(0.0));
   std::cout<< " r " << r << std::endl;
 
   return 0;
