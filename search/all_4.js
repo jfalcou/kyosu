413ccd15caba00f3251a6a62e089009e998535e9
jfalcou <jfalcou@users.noreply.github.com>
2025-01-26 18:57:54 +0000

    deploy: 4f9e43e8bba89fe86e67f8cbf651cd5b34bdbb35

--- search/all_4.js
+++ search/all_4.js
@@ -1,15 +1,20 @@
 var searchData=
 [
-  ['erf_0',['erf',['../group__functions_gacb0340f73510f30b99ad37fa82c22e8f.html#gacb0340f73510f30b99ad37fa82c22e8f',1,'kyosu']]],
-  ['erfcx_1',['erfcx',['../group__functions_ga738d6a61ebf48d0bacd80f958d1b590b.html#ga738d6a61ebf48d0bacd80f958d1b590b',1,'kyosu']]],
-  ['erfi_2',['erfi',['../group__functions_gac01bdb4384827ce21ddddedd19f8b032.html#gac01bdb4384827ce21ddddedd19f8b032',1,'kyosu']]],
-  ['eta_3',['eta',['../group__functions_ga7ad40bb479ce858eed855c2819c4ab22.html#ga7ad40bb479ce858eed855c2819c4ab22',1,'kyosu']]],
-  ['exp_4',['exp',['../group__functions_gad726f439fb9920c055c094631185d449.html#gad726f439fb9920c055c094631185d449',1,'kyosu']]],
-  ['exp10_5',['exp10',['../group__functions_ga9c08281f95af17445823ff698486d24b.html#ga9c08281f95af17445823ff698486d24b',1,'kyosu']]],
-  ['exp2_6',['exp2',['../group__functions_ga0ae8f99de8716a46dd7de3ff990602bb.html#ga0ae8f99de8716a46dd7de3ff990602bb',1,'kyosu']]],
-  ['exp_5fi_7',['exp_i',['../group__functions_gad5a796471bcfd81c38de6db0658fa23a.html#gad5a796471bcfd81c38de6db0658fa23a',1,'kyosu']]],
-  ['exp_5fipi_8',['exp_ipi',['../group__functions_ga761c2a207a0417867f0eb4b3dc30ee2b.html#ga761c2a207a0417867f0eb4b3dc30ee2b',1,'kyosu']]],
-  ['expm1_9',['expm1',['../group__functions_gab0f1c35d5251d1f6db39bec53d8960b6.html#gab0f1c35d5251d1f6db39bec53d8960b6',1,'kyosu']]],
-  ['expmx2_10',['expmx2',['../group__functions_ga29021b202d2832d1e6863d7402e25b79.html#ga29021b202d2832d1e6863d7402e25b79',1,'kyosu']]],
-  ['expx2_11',['expx2',['../group__functions_ga6658defe6cc879802872ceb5e92749a9.html#ga6658defe6cc879802872ceb5e92749a9',1,'kyosu']]]
+  ['ellint_5frc_0',['ellint_rc',['../group__functions_ga76183eda5f2d4054114c9e9e1e68af43.html#ga76183eda5f2d4054114c9e9e1e68af43',1,'kyosu']]],
+  ['ellint_5frd_1',['ellint_rd',['../group__functions_ga4ef45a3ee36d9f1cb692bf38f26108af.html#ga4ef45a3ee36d9f1cb692bf38f26108af',1,'kyosu']]],
+  ['ellint_5frf_2',['ellint_rf',['../group__functions_gae9b1ce4a87f3f7e687f3ee2e04e7385d.html#gae9b1ce4a87f3f7e687f3ee2e04e7385d',1,'kyosu']]],
+  ['ellint_5frg_3',['ellint_rg',['../group__functions_ga87c722e6ca5c350838e02f8989f2ae20.html#ga87c722e6ca5c350838e02f8989f2ae20',1,'kyosu']]],
+  ['ellint_5frj_4',['ellint_rj',['../group__functions_ga53653f456f60372869be158d10d162a5.html#ga53653f456f60372869be158d10d162a5',1,'kyosu']]],
+  ['erf_5',['erf',['../group__functions_gacb0340f73510f30b99ad37fa82c22e8f.html#gacb0340f73510f30b99ad37fa82c22e8f',1,'kyosu']]],
+  ['erfcx_6',['erfcx',['../group__functions_ga738d6a61ebf48d0bacd80f958d1b590b.html#ga738d6a61ebf48d0bacd80f958d1b590b',1,'kyosu']]],
+  ['erfi_7',['erfi',['../group__functions_gac01bdb4384827ce21ddddedd19f8b032.html#gac01bdb4384827ce21ddddedd19f8b032',1,'kyosu']]],
+  ['eta_8',['eta',['../group__functions_ga7ad40bb479ce858eed855c2819c4ab22.html#ga7ad40bb479ce858eed855c2819c4ab22',1,'kyosu']]],
+  ['exp_9',['exp',['../group__functions_gad726f439fb9920c055c094631185d449.html#gad726f439fb9920c055c094631185d449',1,'kyosu']]],
+  ['exp10_10',['exp10',['../group__functions_ga9c08281f95af17445823ff698486d24b.html#ga9c08281f95af17445823ff698486d24b',1,'kyosu']]],
+  ['exp2_11',['exp2',['../group__functions_ga0ae8f99de8716a46dd7de3ff990602bb.html#ga0ae8f99de8716a46dd7de3ff990602bb',1,'kyosu']]],
+  ['exp_5fi_12',['exp_i',['../group__functions_gad5a796471bcfd81c38de6db0658fa23a.html#gad5a796471bcfd81c38de6db0658fa23a',1,'kyosu']]],
+  ['exp_5fipi_13',['exp_ipi',['../group__functions_ga761c2a207a0417867f0eb4b3dc30ee2b.html#ga761c2a207a0417867f0eb4b3dc30ee2b',1,'kyosu']]],
+  ['expm1_14',['expm1',['../group__functions_gab0f1c35d5251d1f6db39bec53d8960b6.html#gab0f1c35d5251d1f6db39bec53d8960b6',1,'kyosu']]],
+  ['expmx2_15',['expmx2',['../group__functions_ga29021b202d2832d1e6863d7402e25b79.html#ga29021b202d2832d1e6863d7402e25b79',1,'kyosu']]],
+  ['expx2_16',['expx2',['../group__functions_ga6658defe6cc879802872ceb5e92749a9.html#ga6658defe6cc879802872ceb5e92749a9',1,'kyosu']]]
 ];
