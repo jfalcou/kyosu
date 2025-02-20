87686a9ece75e04b33a069e5a32df524bf665a20
jtlap <jtlapreste@gmail.com>
2025-02-20 16:23:41 +0100

    kummer test

--- test/unit/function/kummer.cpp
+++ test/unit/function/kummer.cpp
@@ -26,24 +26,46 @@ TTS_CASE_TPL ( "Check kummer"
                r_t(2.68, 0.0),
                r_t(0.07950472902909934, 0.0),
                r_t(-136.227503222143526977, 41.13766180660205530874),
-               r_t(1.08556626293120142, -0.9298264679463321554100)
+               r_t(1.08556626293120142, -0.9298264679463321554100),
+               r_t(-79.62624, 0.0)
   };
-  int i = 0;
-  r_t r =  kyosu::kummer(r_t(0.0), r_t(-20.4), r_t(2.0));
-  TTS_RELATIVE_EQUAL(r, res[i++], pr);
-  r =  kyosu::kummer(r_t(0.4), r_t(-20.4), r_t(2.0));
-  TTS_RELATIVE_EQUAL(r, res[i++], pr);
-  r =  kyosu::kummer(r_t(2.4), r_t(-20.4), r_t(2.0));
-  TTS_RELATIVE_EQUAL(r, res[i++], pr);
-  r =  kyosu::kummer(r_t(2.4), r_t(-20.4), r_t(-2.0));
-  TTS_RELATIVE_EQUAL(r, res[i++], pr);
-  r =  kyosu::kummer(r_t(2.4), r_t(-2), r_t(-4.0));
-  TTS_RELATIVE_EQUAL(r, res[i++], pr);
-  r =  kyosu::kummer(r_t(2.4), r_t(-20.4), r_t(2.0));
-  TTS_RELATIVE_EQUAL(r, res[i++], pr);
-  r =  kyosu::kummer(r_t(0.5+20.4*I), r_t(-2.4), r_t(2.0));
-  TTS_RELATIVE_EQUAL(r, res[i++], pr);
-  r =  kyosu::kummer(r_t(0.5+20.4*I), r_t(-2.4+I), r_t(2.0+5.0*I));
-  TTS_RELATIVE_EQUAL(r, res[i++], pr);
-
+  {
+    r_t r =  kyosu::kummer(r_t(0.0), r_t(-20.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[0], pr);
+    r =  kyosu::kummer(r_t(0.4), r_t(-20.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[1], pr);
+    r =  kyosu::kummer(r_t(2.4), r_t(-20.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[2], pr);
+    r =  kyosu::kummer(r_t(2.4), r_t(-20.4), r_t(-2.0));
+    TTS_RELATIVE_EQUAL(r, res[3], pr);
+    r =  kyosu::kummer(r_t(2.4), r_t(-2), r_t(-4.0));
+    TTS_RELATIVE_EQUAL(r, res[4], pr);
+    r =  kyosu::kummer(r_t(2.4), r_t(-20.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[5], pr);
+    r =  kyosu::kummer(r_t(0.5+20.4*I), r_t(-2.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[6], pr);
+    r =  kyosu::kummer(r_t(0.5+20.4*I), r_t(-2.4+I), r_t(2.0+5.0*I));
+    TTS_RELATIVE_EQUAL(r, res[7], pr);
+  }
+  {
+    using kyosu::regularized;
+    r_t r =  kyosu::kummer[regularized](r_t(0.0), r_t(-20.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[0]*kyosu::tgamma_inv(2.0), pr);
+    r =  kyosu::kummer[regularized](r_t(0.4), r_t(-20.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[1]*kyosu::tgamma_inv(2.0), pr);
+    r =  kyosu::kummer[regularized](r_t(2.4), r_t(-20.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[2]*kyosu::tgamma_inv(2.0), pr);
+    r =  kyosu::kummer[regularized](r_t(2.4), r_t(-20.4), r_t(-2.0));
+    TTS_RELATIVE_EQUAL(r, res[3], pr);
+    r =  kyosu::kummer[regularized](r_t(2.4), r_t(-2), r_t(-4.0));
+    TTS_RELATIVE_EQUAL(r, res[4]*kyosu::tgamma_inv(-2.0), pr);
+    r =  kyosu::kummer[regularized](r_t(2.4), r_t(-20.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[5]*kyosu::tgamma_inv(2.0), pr);
+    r =  kyosu::kummer[regularized](r_t(0.5+20.4*I), r_t(-2.4), r_t(2.0));
+    TTS_RELATIVE_EQUAL(r, res[6]*kyosu::tgamma_inv(2.0), pr);
+    r =  kyosu::kummer[regularized](r_t(0.5+20.4*I), r_t(-2.4+I), r_t(2.0+5.0*I));
+    TTS_RELATIVE_EQUAL(r, res[7]*kyosu::tgamma_inv(2.0+5.0*I), pr);
+    r =  kyosu::kummer[regularized](r_t(2.4), r_t(-5), r_t(-4.0));
+    TTS_RELATIVE_EQUAL(r, res[8], pr);
+  }
 };
