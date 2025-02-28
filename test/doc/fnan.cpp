a06bcdba1a75c60af8c7a2e6990c59ba0f8a4c5b
jtlap <jtlapreste@gmail.com>
2025-02-28 11:54:25 +0100

    More docs and doc tests

---
+++ test/doc/fnan.cpp
@@ -0,0 +1,37 @@
+#include <kyosu/kyosu.hpp>
+#include <eve/wide.hpp>
+#include <iostream>
+
+int main()
+{
+  using kyosu::as;
+  using kyosu::complex_t;
+  using kyosu::quaternion_t;
+
+  std::cout << "Real:        \n";
+  auto fnan = kyosu::fnan(kyosu::as(72.9f));
+  std::cout << "fnan  = "  << fnan << "\n";
+  std::cout << "rec(fnan) = " << kyosu::rec(fnan) << std::endl;
+  std::cout << "1.0f/fnan = " << 1.0/fnan << std::endl;
+  std::cout << 1.0f/kyosu::complex_t<float>(0.0, 0.0) << std::endl;
+  std::cout << "kyosu::abs(fnan) " << kyosu::abs(fnan) << std::endl;
+  std::cout << "kyosu::arg(fnan) " << kyosu::arg(fnan) << std::endl;
+  std::cout << "Complex:     \n";
+  auto c = kyosu::complex_t<float>(3.5f,-2.9f);
+  std::cout << "c       = " << c << std::endl;
+  std::cout << "fnan(as(c)) " << " -> " << kyosu::fnan(as(c)) << "\n";
+  std::cout << c/kyosu::complex_t<float>(0.0, 0.0) << std::endl;
+  std::cout << c/0.0f << std::endl;
+
+  std::cout << "Quaternion:  \n";
+  auto z = kyosu::quaternion_t<double>(1.,2.,3.,4.);
+  std::cout <<  "fnan(as(z)) " << " -> " <<  kyosu::fnan(as(z)) << "\n";
+
+  std::cout << "SIMD:        ";
+  using wc_t = eve::wide<kyosu::complex_t<double>, eve::fixed<2>>;
+  auto wz =  wc_t(complex_t<double>(1.3,-3.7));
+  std::cout << "fnan(as(wz)) " << " -> " << kyosu::fnan(as(wz)) << "\n";
+
+
+  return 0;
+}
---
+++ test/doc/is_not_flint.cpp
@@ -0,0 +1,48 @@
+#include <kyosu/kyosu.hpp>
+#include <eve/wide.hpp>
+#include <iostream>
+
+int main()
+{
+  using kyosu::is_not_flint;
+  using kyosu::complex_t;
+  using kyosu::quaternion_t;
+  using eve::as;
+  using e_t = float;
+  using c_t = kyosu::complex_t<float>;
+  using q_t = kyosu::quaternion_t<float>;
+  using we_t = eve::wide<e_t, eve::fixed<4>>;
+  using wc_t = eve::wide<c_t, eve::fixed<4>>;
+  using wq_t = eve::wide<q_t, eve::fixed<4>>;
+
+  std::cout << "Real:        \n";
+  e_t e(2.9f);
+  e_t nan = eve::nan(as(e));
+  e_t inf = eve::inf(as(e));
+  e_t zer = eve::zero(as(e));
+  we_t we = we_t(e, zer, nan, inf);
+  std::cout << e << " -> " << is_not_flint(e) << "\n";
+  std::cout << we << " -> " << is_not_flint(we) << "\n";
+  std::cout               << is_not_flint(c_t(e))<< "\n";
+  std::cout               << is_not_flint(q_t(e))<< "\n";
+  std::cout               << is_not_flint(kyosu::complex(we))<< "\n";
+  std::cout               << is_not_flint(kyosu::quaternion(we))<< "\n";
+
+  std::cout << "Complex:     \n";
+  c_t c(3.0f,-0.0f);
+  c_t d(5.0f, 2.0f);
+  wc_t wc = wc_t(c, zer, nan, d);
+  std::cout << c << " -> " << is_not_flint(c) << "\n";
+  std::cout << wc << " -> " << is_not_flint(wc) << "\n";
+  std::cout                 << is_not_flint(kyosu::complex(wc))<< "\n";;
+  std::cout                 << is_not_flint(kyosu::quaternion(wc))<< "\n";
+
+  std::cout << "Quaternion:  \n";
+  q_t q(3.5f,-2.9f, 2.0f, 3.2f);
+  q_t r(3.5f, nan, inf, zer);
+  wq_t wq = wq_t(q, zer, nan, r);
+  std::cout << q << " -> " << is_not_flint(q) << "\n";
+  std::cout << wq << " -> " << is_not_flint(wq) << "\n";
+
+  return 0;
+}
