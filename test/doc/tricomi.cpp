7683a6e2fc4d5561e71b5f8023f4307d657a54af
jtlap <jtlapreste@gmail.com>
2025-03-07 09:24:29 +0100

    tricomi still have pbs

--- test/doc/tricomi.cpp
+++ test/doc/tricomi.cpp
@@ -4,16 +4,16 @@
 
 int main()
 {
-  auto eps = eve::eps(eve::as(1.0))*1024;
-  auto ops = 1.0+eps;
- auto I = kyosu::complex(0.0, 1.0);
-//   std::cout << "tricomi(2.0, 1, 1.5)         "<< kyosu::tricomi(2.0, 1, 1.5)        << std::endl;
-//   std::cout << "tricomi(2.0, 1, I)           "<< kyosu::tricomi(2.0, 1, I)          << std::endl;
-  std::cout << "tricomi( 1.72725307941437, 1.0, 1.0)  "<< kyosu::tricomi( 1.72725307941437, 1.0, 1.0)        << std::endl;
-  std::cout << "tricomi( 1.72725307941437, 1.0, 1.0+I*eps)  "<< kyosu::tricomi( 1.72725307941437, 1.0, 1.0+I*eps)        << std::endl;
-  std::cout << "tricomi( 1.72725307941437, 1.0, ops)  "<< kyosu::tricomi( 1.72725307941437, 1.0, ops)        << std::endl;
-  std::cout << "tricomi( 1.72725307941437, 1., 1.00001)  "<< kyosu::tricomi( 1.72725307941437, 1., 1.00001)        << std::endl;
-//   std::cout << "tricomi( 1.72725307941437, -1.0, -1.0)  "<< kyosu::tricomi( 1.72725307941437, -1.0, -1.0)        << std::endl;
-//   std::cout << "tricomi( 1.72725307941437, -1.00001, -1.00001)  "<< kyosu::tricomi( 1.72725307941437, -1.00001, -1.00001)        << std::endl;
+  auto I = kyosu::complex(0.0, 1.0);
+  std::cout << "tricomi(2.0, 1, 1.5)         "<< kyosu::tricomi(2.0, 1, 1.5)        << std::endl;
+  std::cout << "tricomi(2.0, 1, I)           "<< kyosu::tricomi(2.0, 1, I)          << std::endl;
+  std::cout << "tricomi( 1.72725307941437, 0.0, 0.0)  "<< kyosu::tricomi( 1.72725307941437, 0.0, 0.0) << std::endl;
+  std::cout << "tricomi( 1.72725307941437, 1.0, 1.0)  "<< kyosu::tricomi( 1.72725307941437, 1.0, 1.0) << std::endl;
+  std::cout << "tricomi( 1.72725307941437, 2.0, 2.0)  "<< kyosu::tricomi( 1.72725307941437, 2.0, 2.0) << std::endl;
+  std::cout << "tricomi( 1.72725307941437, 3.0, 3.0)  "<< kyosu::tricomi( 1.72725307941437, 3.0, 3.0) << std::endl;
+  std::cout << "tricomi( 1.72725307941437+I, 0.0, 0.0)  "<< kyosu::tricomi( 1.72725307941437+I, 0.0, 0.0) << std::endl;
+  std::cout << "tricomi( 1.72725307941437+I, 1.0, 1.0)  "<< kyosu::tricomi( 1.72725307941437+I, 1.0, 1.0) << std::endl;
+  std::cout << "tricomi( 1.72725307941437+I, 2.0, 2.0)  "<< kyosu::tricomi( 1.72725307941437+I, 2.0, 2.0) << std::endl;
+  std::cout << "tricomi( 1.72725307941437+I, 3.0, 3.0)  "<< kyosu::tricomi( 1.72725307941437+I, 3.0, 3.0) << std::endl;
   return 0;
 }
