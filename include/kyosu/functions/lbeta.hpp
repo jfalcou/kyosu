96a734e95df86d0c60e8c9b137e69b2df17219c9
jtlap <jtlapreste@gmail.com>
2025-03-10 23:00:00 +0100

    lbeta from log_gamma

--- include/kyosu/functions/lbeta.hpp
+++ include/kyosu/functions/lbeta.hpp
@@ -8,8 +8,7 @@
 #pragma once
 #include <kyosu/details/callable.hpp>
 #include <kyosu/functions/to_complex.hpp>
-#include <kyosu/functions/log.hpp>
-#include <kyosu/functions/beta.hpp>
+#include <kyosu/functions/log_gamma.hpp>
 
 namespace kyosu
 {
@@ -19,7 +18,7 @@ namespace kyosu
     template<typename Z0, typename Z1>
     requires(concepts::cayley_dickson<Z0> || concepts::cayley_dickson<Z1>)
       KYOSU_FORCEINLINE constexpr as_cayley_dickson_t<Z0, Z1>  operator()(Z0 const& z0, Z1 const & z1) const noexcept
-    { return kyosu::log(kyosu::beta(z0, z1)); }
+    { return kyosu::log_gamma(z0)+log_gamma(z1)-log_gamma(z0+z1); }
 
     template<concepts::real V0, concepts::real V1>
     KYOSU_FORCEINLINE constexpr auto operator()(V0 v0, V1 v1) const noexcept -> decltype(complex(v0)+v1)
