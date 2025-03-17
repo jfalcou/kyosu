48e0d3206c8ee2e58b8904b58a3edd9e82e8c517
jfalcou <jfalcou@users.noreply.github.com>
2025-03-17 15:42:15 +0000

    deploy: 0b3f1ddb9c25f66a3bee3237b9c5aa26b9fe582c

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['cayley_5fdickson_0',['cayley_dickson',['../structkyosu_1_1cayley__dickson.html',1,'kyosu']]]
+  ['cayley_5fdickson_0',['cayley_dickson',['../structkyosu_1_1cayley__dickson.html',1,'kyosu']]],
+  ['complexify_1',['complexify',['../structkyosu_1_1complexify.html',1,'kyosu']]]
 ];
