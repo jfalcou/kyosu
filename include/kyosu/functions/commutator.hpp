8934193c43bcfe451a6964144f13eff0a6b85afe
jtlap <jtlapreste@gmail.com>
2025-03-21 15:48:41 +0100

    chi commutator conj

--- include/kyosu/functions/commutator.hpp
+++ include/kyosu/functions/commutator.hpp
@@ -13,16 +13,18 @@ namespace kyosu
   template<typename Options>
   struct commutator_t : eve::strict_elementwise_callable<commutator_t, Options>
   {
-    template<typename Z0, typename Z1>
-    requires(concepts::cayley_dickson<Z0> || concepts::cayley_dickson<Z1>)
-    KYOSU_FORCEINLINE constexpr auto  operator()(Z0 const& z0, Z1 const & z1) const noexcept -> decltype(z0+z1)
+    template<concepts::cayley_dickson_like Z0, concepts::cayley_dickson_like Z1>
+    requires(eve::same_lanes_or_scalar<Z0, Z1>)
+      KYOSU_FORCEINLINE constexpr auto  operator()(Z0 const& z0, Z1 const & z1) const
+      noexcept -> decltype(z0+z1)
     {
-      return KYOSU_CALL(z0,z1);
+      constexpr size_t dZ0 = dimension_v<Z0>;
+      constexpr size_t dZ1 = dimension_v<Z1>;
+      if constexpr((dZ0 < 4 && dZ1 < 4) || (dZ0 <= 1 || dZ1 <= 1))
+        return  eve::zero(eve::as<decltype(z0+z1)>());
+      else
+        return (z0*z1) - (z1*z0);
     }
-
-    template<concepts::real V0, concepts::real V1>
-    KYOSU_FORCEINLINE constexpr auto operator()(V0 v0, V1 v1) const noexcept -> decltype(v0+v1)
-    { return eve::zero(eve::as<decltype(v0+v1)>()); }
 
     KYOSU_CALLABLE_OBJECT(commutator_t, commutator_);
 };
@@ -44,17 +46,17 @@ namespace kyosu
 //!   @code
 //!   namespace kyosu
 //!   {
-//!     constexpr auto commutator(auto z0, auto z1) noexcept;
+//!     constexpr auto commutator(auto z0, auto z1)           noexcept;
 //!   }
 //!   @endcode
 //!
 //!   **Parameters**
 //!
-//!     * `z0`, `z1`: Values to process. Can be a mix of complex and real floating values and complex values.
+//!     * `z0`, `z1`: Values to process. Can be a mix of cayley_dickson_like values.
 //!
 //!   **Return value**
 //!
-//!     Returns the difference z0*z1-z1*z0.
+//!     Returns the difference z0*z1-z1*z0. (always exactly zero up to complex)
 //!
 //!  @groupheader{External references}
 //!   *  [Wolfram MathWorld: Commutator](https://mathworld.wolfram.com/Commutator.html)
@@ -68,22 +70,4 @@ namespace kyosu
 //======================================================================================================================
 //! @}
 //======================================================================================================================
-}
-
-namespace kyosu::_
-{
-  template<typename C0, typename C1, eve::callable_options O>
-  KYOSU_FORCEINLINE constexpr auto commutator_(KYOSU_DELAY(), O const&, C0 c0, C1 c1) noexcept
-  {
-    constexpr size_t dC0 = dimension_v<C0>;
-    constexpr size_t dC1 = dimension_v<C1>;
-    if constexpr((dC0 < 4 && dC1 < 4) || (dC0 == 1 || dC1 == 1))
-    {
-      return  eve::zero(eve::as<decltype(c0+c1)>());
-    }
-    else
-    {
-      return c0*c1 - c1*c0;
-    }
-  }
 }
