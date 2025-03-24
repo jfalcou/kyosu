060e3a134342a92f5089e85c7afa26cab8c2958c
jtlap <jtlapreste@gmail.com>
2025-03-24 17:33:43 +0100

    is_equal/not_equal + dist

--- include/kyosu/functions/is_equal.hpp
+++ include/kyosu/functions/is_equal.hpp
@@ -15,19 +15,14 @@ namespace kyosu
   template<typename Options>
   struct is_equal_t : eve::strict_elementwise_callable<is_equal_t, Options, eve::numeric_option>
   {
-    template<typename Z0, typename Z1>
-    requires(concepts::cayley_dickson<Z0> || concepts::cayley_dickson<Z1>)
-      KYOSU_FORCEINLINE constexpr auto  operator()(Z0 const& z0, Z1 const & z1) const noexcept -> eve::as_logical_t<decltype(z0 + z1)>
+    template<concepts::cayley_dickson_like Z0, concepts::cayley_dickson_like Z1>
+      KYOSU_FORCEINLINE constexpr auto  operator()(Z0 const& z0, Z1 const & z1) const noexcept -> decltype(z0 == z1)
     {
       if constexpr(Options::contains(eve::numeric))
-        return (z0 == z1) || (is_nan(z0) && is_nan(z1));
+        return kumi::map([](auto a,  auto b) { return eve::is_equal[eve::numeric](a, b); }, r_t(z0), r_t(z1));
       else
         return z0 == z1;
     }
-
-    template<concepts::real V0, concepts::real V1>
-    KYOSU_FORCEINLINE constexpr auto operator()(V0 v0, V1 v1) const noexcept -> eve::as_logical_t<decltype(v0+ v1)>
-    { return eve::is_equal[this->options()](v0, v1); }
 
     KYOSU_CALLABLE_OBJECT(is_equal_t, is_equal_);
   };
@@ -64,7 +59,7 @@ namespace kyosu
 //!   **Return value**
 //!
 //!     1. Returns elementwise true or false according the equality of the parameters
-//!     2. NaN values are considered equal
+//!     2. NaN values in correspoding parts are considered equal
 //!
 //!  @groupheader{Example}
 //!
