030549e308d1abf087d6e7ba418158ed7e34943c
jtlap <jtlapreste@gmail.com>
2025-03-25 07:58:24 +0100

    beta

--- include/kyosu/functions/beta.hpp
+++ include/kyosu/functions/beta.hpp
@@ -17,15 +17,11 @@ namespace kyosu
   {
     template<typename Z0, typename Z1>
     requires(concepts::cayley_dickson<Z0> || concepts::cayley_dickson<Z1>)
-    KYOSU_FORCEINLINE constexpr as_cayley_dickson_t<Z0, Z1> operator()(Z0 const& z0, Z1 const & z1) const noexcept
+    KYOSU_FORCEINLINE constexpr auto operator()(Z0 const& z0, Z1 const & z1) const noexcept -> complexify_t<decltype(z0+z1)>
     {
-      auto y = z0 + z1;
-      return tgamma(z0)*tgamma(z1)/tgamma(y);
+      if constexpr(concepts::real<Z0> && concepts::real<Z1>) return (*this)(complex(z0), complex(z1));
+      else return KYOSU_CALL(z0, z1);
     }
-
-    template<concepts::real V0, concepts::real V1>
-    KYOSU_FORCEINLINE constexpr auto operator()(V0 v0, V1 v1) const noexcept ->  decltype(beta(complex(v0),v1))
-    { return (*this)(complex(v0),complex(v1)); }
 
     KYOSU_CALLABLE_OBJECT(beta_t, beta_);
 };
@@ -71,4 +67,19 @@ namespace kyosu
 //======================================================================================================================
 //! @}
 //======================================================================================================================
+}
+
+
+namespace kyosu::_
+{
+  template<typename Z0, typename Z1, eve::callable_options O>
+  KYOSU_FORCEINLINE constexpr auto beta_(KYOSU_DELAY(), O const&, Z0 z0, Z1 z1) noexcept
+  {
+    if constexpr(kyosu::concepts::complex<Z0> && kyosu::concepts::complex<Z1>)
+    {
+      auto y = z0 + z1;
+      return tgamma(z0)*tgamma(z1)/tgamma(y);
+    }
+    else return kyosu::_::cayley_extend2(kyosu::beta, z0, z1);
+  }
 }
