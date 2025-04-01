556dbd9d71305c6e7192c02b032ddb410ecb23d1
jtlap <jtlapreste@gmail.com>
2025-04-01 16:42:59 +0200

    Using new concepts: manhattan muli mulmi

--- test/doc/muli.cpp
+++ test/doc/muli.cpp
@@ -13,12 +13,14 @@ int main()
 
   std::cout
     << "---- simd" << std::endl
-    << "<- r             = " << r << std::endl
-    << "-> muli(r)       = " << kyosu::muli(r)<< std::endl
-    << "<- zc            = " << zc << std::endl
-    << "-> muli(zc)      = " << kyosu::muli(zc)<< std::endl
-    << "<- zq            = " << zq << std::endl
-    << "-> muli(zq)      = " << kyosu::muli(zq) << std::endl;
+    << "<- r                         = " << r << std::endl
+    << "-> muli(r)                   = " << kyosu::muli(r)<< std::endl
+    << "<- zc                        = " << zc << std::endl
+    << "-> muli(zc)                  = " << kyosu::muli(zc)<< std::endl
+    << "<- zq                        = " << zq << std::endl
+    << "-> muli(zq)                  = " << kyosu::muli(zq) << std::endl
+    << "-> muli[eve::left](zq)       = " << kyosu::muli[eve::left](zq) << std::endl
+    << "-> muli[eve::right](zq)      = " << kyosu::muli[eve::right](zq) << std::endl;
 
   return 0;
 }
