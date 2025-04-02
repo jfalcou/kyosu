31c843f99e2fbf97fd5ebdd8b73974955f46f0bf
jfalcou <jfalcou@users.noreply.github.com>
2025-04-02 20:30:36 +0000

    deploy: 6524014fcf9121104fc1468bd992bed43aceba3e

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['as_5freal_0',['as_real',['../structkyosu_1_1as__real.html',1,'kyosu']]]
+  ['as_5fcayley_5fdickson_5flike_0',['as_cayley_dickson_like',['../structkyosu_1_1as__cayley__dickson__like.html',1,'kyosu']]],
+  ['as_5freal_1',['as_real',['../structkyosu_1_1as__real.html',1,'kyosu']]]
 ];
