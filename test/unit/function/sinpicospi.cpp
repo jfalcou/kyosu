3ce46cb2e68bb459ab698dc102ef881bf19c87c5
jtlap <jtlapreste@gmail.com>
2025-04-03 09:59:43 +0200

    sinpicospi test

--- test/unit/function/sinpicospi.cpp
+++ test/unit/function/sinpicospi.cpp
@@ -18,21 +18,21 @@ TTS_CASE_WITH ( "Check kyosu::sin over quaternion"
 {
   {
     auto [s, c] = kyosu::sinpicospi(r);
-    TTS_RELATIVE_EQUAL(s, kyosu::sin(r), tts::prec<T>());
-    TTS_RELATIVE_EQUAL(c, kyosu::cos(r), tts::prec<T>());
+    TTS_RELATIVE_EQUAL(s, kyosu::sinpi(r), tts::prec<T>());
+    TTS_RELATIVE_EQUAL(c, kyosu::cospi(r), tts::prec<T>());
   }
   {
     using ke_t = kyosu::complex_t<T>;
     auto cq= ke_t(r,i);
     auto [s, c] = kyosu::sinpicospi(cq);
-    TTS_RELATIVE_EQUAL(s, kyosu::sin(cq), tts::prec<T>());
-    TTS_RELATIVE_EQUAL(c, kyosu::cos(cq), tts::prec<T>());
+    TTS_RELATIVE_EQUAL(s, kyosu::sinpi(cq), tts::prec<T>());
+    TTS_RELATIVE_EQUAL(c, kyosu::cospi(cq), tts::prec<T>());
   }
   {
     using ke_t = kyosu::quaternion_t<T>;
     auto q = ke_t(r,i,j,k);
     auto [s, c] = kyosu::sinpicospi(q);
-    TTS_RELATIVE_EQUAL(s, kyosu::sin(q), tts::prec<T>());
-    TTS_RELATIVE_EQUAL(c, kyosu::cos(q), tts::prec<T>());
+    TTS_RELATIVE_EQUAL(s, kyosu::sinpi(q), tts::prec<T>());
+    TTS_RELATIVE_EQUAL(c, kyosu::cospi(q), tts::prec<T>());
   }
 };
