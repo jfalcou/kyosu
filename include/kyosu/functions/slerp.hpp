081dc67a9e92f91f71fd181822cc9b21aeba35ae
jtlap <jtlapreste@gmail.com>
2025-04-09 18:59:32 +0200

    slerp sqr/abs - clean up

--- include/kyosu/functions/slerp.hpp
+++ include/kyosu/functions/slerp.hpp
@@ -18,25 +18,13 @@ namespace kyosu
   template<typename Options>
   struct slerp_t : eve::strict_elementwise_callable<slerp_t, Options, assume_unitary_option>
   {
-    template<typename Z0, typename Z1, typename Z2>
-    requires((concepts::cayley_dickson<Z0> || concepts::cayley_dickson<Z1>)&& concepts::real<Z2>)
-    KYOSU_FORCEINLINE constexpr auto operator()(Z0 z0, Z1 z1, Z2  z2) const noexcept
-    -> decltype(z0+z1+z2)
+    template<concepts::cayley_dickson_like Z0, concepts::cayley_dickson_like Z1, concepts::real Z2>
+    requires(eve::same_lanes_or_scalar<Z0, Z1, Z2>)
+    KYOSU_FORCEINLINE constexpr  kyosu::as_cayley_dickson_like_t<complexify_t<Z0>,complexify_t<Z1>,Z2>  operator()(Z0 const& c0, Z1 const & c1, Z2 const & c2) const
+      noexcept
     {
-      if constexpr(!Options::contains(assume_unitary))
-      {
-        z0 = kyosu::signnz(z0);
-        z1 = kyosu::signnz(z1);
-      }
-      auto gez = eve::is_gez(real(kyosu::dot(z0, z1)));
-      auto mix = kyosu::if_else(gez, z1, -z1);
-      return z1*kyosu::pow(kyosu::conj(z1)*mix, z2);
+        return KYOSU_CALL(c0, c1, c2);
     }
-
-    template<concepts::real V0, concepts::real V1, concepts::real V2>
-    KYOSU_FORCEINLINE constexpr auto operator()(V0 v0, V1 v1, V2 v2) const noexcept
-    -> decltype(complex(v0)+complex(v1)+v2)
-    { return (*this)(complex(v0),complex(v1),v2); }
 
     KYOSU_CALLABLE_OBJECT(slerp_t, slerp_);
 };
@@ -58,7 +46,7 @@ namespace kyosu
 //!   @code
 //!   namespace kyosu
 //!   {
-//!     constexpr auto slerp(auto z0, auto z1, floating_ordered_value t) noexcept;
+//!     constexpr auto slerp(auto z0, auto z1, concepts::real t) noexcept;
 //!   }
 //!   @endcode
 //!
@@ -82,4 +70,22 @@ namespace kyosu
 //======================================================================================================================
 //! @}
 //======================================================================================================================
+}
+
+namespace kyosu::_
+{
+  template<typename Z0, typename Z1, typename Z2, eve::callable_options O>
+  KYOSU_FORCEINLINE constexpr auto slerp_(KYOSU_DELAY(), O const&, Z0 z0, Z1 z1, Z2 z2) noexcept
+  {
+    if constexpr(concepts::real<Z0> && concepts::real<Z1>)
+      return kyosu::slerp(complex(z0),complex(z1),z2);
+    else if constexpr(!O::contains(assume_unitary))
+    {
+      z0 = kyosu::signnz(z0);
+      z1 = kyosu::signnz(z1);
+    }
+    auto gez = eve::is_gez(real(kyosu::dot(z0, z1)));
+    auto mix = kyosu::if_else(gez, z1, -z1);
+    return z1*kyosu::pow(kyosu::conj(z1)*mix, z2);
+  }
 }
