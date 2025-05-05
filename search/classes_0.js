8d43c509463eb91cce69150aaa71446f80be3eb1
jfalcou <jfalcou@users.noreply.github.com>
2025-05-05 19:51:30 +0000

    deploy: dd4dba87ec42da9d2fa1f8714505a6f19893872b

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['as_5fcayley_5fdickson_5flike_0',['as_cayley_dickson_like',['../structkyosu_1_1as__cayley__dickson__like.html',1,'kyosu']]],
-  ['as_5freal_1',['as_real',['../structkyosu_1_1as__real.html',1,'kyosu']]]
+  ['as_5fcayley_5fdickson_5flike_3c_20t_2c_20kumi_3a_3aapply_5ftraits_5ft_3c_20as_5fcayley_5fdickson_5flike_2c_20ts_20_3e_20_3e_1',['as_cayley_dickson_like&lt; T, kumi::apply_traits_t&lt; as_cayley_dickson_like, Ts &gt; &gt;',['../structkyosu_1_1as__cayley__dickson__like.html',1,'kyosu']]],
+  ['as_5freal_2',['as_real',['../structkyosu_1_1as__real.html',1,'kyosu']]]
 ];
