c5351c433c8b2dcb7a85bdd2f938158559d0d0c9
jtlap <jtlapreste@gmail.com>
2025-07-21 10:56:59 +0200

    clening doc test

--- test/doc/kronecker.cpp
+++ test/doc/kronecker.cpp
@@ -5,14 +5,10 @@
 int main()
 {
   using kyosu::kronecker;
-//   using kyosu::complex_t;
-//   using kyosu::quaternion_t;
-   using e_t = float;
-   using c_t = kyosu::complex_t<float>;
-//   using q_t = kyosu::quaternion_t<float>;
-   using we_t = eve::wide<float, eve::fixed<2>>;
-   using wc_t = eve::wide<kyosu::complex_t<float>, eve::fixed<2>>;
-//   using wq_t = eve::wide<kyosu::quaternion_t<float>, eve::fixed<2>>;
+  using e_t = float;
+  using c_t = kyosu::complex_t<float>;
+  using we_t = eve::wide<float, eve::fixed<2>>;
+  using wc_t = eve::wide<kyosu::complex_t<float>, eve::fixed<2>>;
 
   std::cout << "Real:        "<< "\n";
   e_t e0(3);
@@ -38,28 +34,5 @@ int main()
   wc_t wc1(c1, c1);
   std::cout << wc0 << ", " << wc1 << " -> " << kronecker(wc0, wc1) << "\n";
 
-//   std::cout << "Quaternion:  "<< "\n";
-//   q_t q0(1, 5, 2, 3);
-//   q_t q1(5, 9, 6, 7);
-//   std::cout << q0 << ", " << q1 << " -> "  << kronecker(q0, q1) << "\n";
-//   std::cout << q0 << ", " << q0 << " -> "  << kronecker(q0, q0) << "\n";
-//   std::cout << q0 << ", " << q0 << ", " << q1 << " -> "  << kronecker(q0, q0, q1) << "\n";
-//   wq_t wq0(q0, q1);
-//   wq_t wq1(q1, q1);
-//   std::cout << wq0 << ", " << wq1 << " -> " << kronecker(wq0, wq1) << "\n";
-
-//   std::cout << "Mixed:  "<< "\n";
-//   std::cout << kyosu::kronecker(c0, e0) << std::endl;
-//   std::cout << kyosu::kronecker(c0, c0, c1) << std::endl;
-//   std::cout << kyosu::kronecker(c0, e0, c1) << std::endl;
-//   std::cout << kyosu::kronecker(c0, q1, e0) << std::endl;
-//   std::cout << kyosu::kronecker(e0, q1, c1) << std::endl;
-//   std::cout << kyosu::kronecker(c0, wq1) << std::endl;
-//   std::cout << kyosu::kronecker(we0, q1) << std::endl;
-
-//   std::cout << c0 <<  " == " <<  e0 << " == " << kyosu::kronecker[e0 > 32](c0, e0) << std::endl;
-//   std::cout << c0 <<  " == " <<  e0 << " == " << kyosu::kronecker[e0 <  32](c0, e0) << std::endl;
-//   kumi::tuple s{c0, c0, c1};
-//   std::cout <<  "kyosu::kronecker( kumi::tuple s{c0, c0, c1}) == " << kyosu::kronecker(s) << std::endl;
   return 0;
 }
