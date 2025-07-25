25ba6e2844995f08b1931df333e124824f0c444b
jfalcou <jfalcou@users.noreply.github.com>
2025-07-25 16:47:42 +0000

    deploy: 61310b83e0e61640842a7d1ce251720fc392521c

--- search/all_a.js
+++ search/all_a.js
@@ -4,10 +4,11 @@ var searchData=
   ['complex_1',['complex',['../conceptkyosu_1_1concepts_1_1complex.html',1,'kyosu::concepts']]],
   ['k_2',['k',['../group__constants_gaa1ba802b81aac3ea60fdbd1fa551c20e.html#gaa1ba802b81aac3ea60fdbd1fa551c20e',1,'kyosu']]],
   ['kpart_3',['kpart',['../group__functions_ga057662e8933c9837bb4bbb5ffb2822f0.html#ga057662e8933c9837bb4bbb5ffb2822f0',1,'kyosu']]],
-  ['kummer_4',['kummer',['../group__functions_ga83edd56ee60fedf9154fe07bad9cc580.html#ga83edd56ee60fedf9154fe07bad9cc580',1,'kyosu']]],
-  ['kyosu_5',['Kyosu',['../index.html',1,'']]],
-  ['kyosu_6',['kyosu',['../namespacekyosu.html',1,'']]],
-  ['octonion_7',['octonion',['../conceptkyosu_1_1concepts_1_1octonion.html',1,'kyosu::concepts']]],
-  ['quaternion_8',['quaternion',['../conceptkyosu_1_1concepts_1_1quaternion.html',1,'kyosu::concepts']]],
-  ['real_9',['real',['../conceptkyosu_1_1concepts_1_1real.html',1,'kyosu::concepts']]]
+  ['kronecker_4',['kronecker',['../namespacekyosu_abba63e00b17a22a27b1784497338f852.html#abba63e00b17a22a27b1784497338f852',1,'kyosu']]],
+  ['kummer_5',['kummer',['../group__functions_ga83edd56ee60fedf9154fe07bad9cc580.html#ga83edd56ee60fedf9154fe07bad9cc580',1,'kyosu']]],
+  ['kyosu_6',['Kyosu',['../index.html',1,'']]],
+  ['kyosu_7',['kyosu',['../namespacekyosu.html',1,'']]],
+  ['octonion_8',['octonion',['../conceptkyosu_1_1concepts_1_1octonion.html',1,'kyosu::concepts']]],
+  ['quaternion_9',['quaternion',['../conceptkyosu_1_1concepts_1_1quaternion.html',1,'kyosu::concepts']]],
+  ['real_10',['real',['../conceptkyosu_1_1concepts_1_1real.html',1,'kyosu::concepts']]]
 ];
