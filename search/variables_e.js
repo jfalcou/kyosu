279d23e368c170ec77c7558aa55c86d99196bdd5
jfalcou <jfalcou@users.noreply.github.com>
2025-08-07 21:25:16 +0000

    deploy: e465b68cab1e1189d0d6bcddf28b3f3fee580392

--- search/variables_e.js
+++ search/variables_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['oneminus_0',['oneminus',['../group__functions_gae8a89a63722b9cdec2f2d4daf58bf025.html#gae8a89a63722b9cdec2f2d4daf58bf025',1,'kyosu']]]
+  ['omega_0',['omega',['../group__functions_ga5827a54e125c3d13150e4bb5f4444cf8.html#ga5827a54e125c3d13150e4bb5f4444cf8',1,'kyosu']]],
+  ['oneminus_1',['oneminus',['../group__functions_gae8a89a63722b9cdec2f2d4daf58bf025.html#gae8a89a63722b9cdec2f2d4daf58bf025',1,'kyosu']]]
 ];
