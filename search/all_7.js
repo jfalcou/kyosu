98968bf98db38ae17f24101286a01f90b93cdf4a
jfalcou <jfalcou@users.noreply.github.com>
2026-01-14 13:26:56 +0000

    deploy: 10f0fc7ff51539f7087ea163ad6f150950b4f32f

--- search/all_7.js
+++ search/all_7.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['horner_0',['horner',['../group__functions_ga002c8ed4aedb1a6523be122842f526ab.html#ga002c8ed4aedb1a6523be122842f526ab',1,'kyosu']]],
-  ['hypergeometric_1',['hypergeometric',['../group__functions_ga6882759ffa560bc8645093bcad7fe7f5.html#ga6882759ffa560bc8645093bcad7fe7f5',1,'kyosu']]],
-  ['hypot_2',['hypot',['../group__functions_gaf0240aea3dcf4428abd35906e94323e7.html#gaf0240aea3dcf4428abd35906e94323e7',1,'kyosu']]]
+  ['harmmean_0',['harmmean',['../group__functions_ga03b104287f72e06e808ab07e78c8391e.html#ga03b104287f72e06e808ab07e78c8391e',1,'kyosu']]],
+  ['horner_1',['horner',['../group__functions_ga002c8ed4aedb1a6523be122842f526ab.html#ga002c8ed4aedb1a6523be122842f526ab',1,'kyosu']]],
+  ['hypergeometric_2',['hypergeometric',['../group__functions_ga6882759ffa560bc8645093bcad7fe7f5.html#ga6882759ffa560bc8645093bcad7fe7f5',1,'kyosu']]],
+  ['hypot_3',['hypot',['../group__functions_gaf0240aea3dcf4428abd35906e94323e7.html#gaf0240aea3dcf4428abd35906e94323e7',1,'kyosu']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['k_0',['k',['../group__constants_gaa1ba802b81aac3ea60fdbd1fa551c20e.html#gaa1ba802b81aac3ea60fdbd1fa551c20e',1,'kyosu']]],
-  ['kpart_1',['kpart',['../group__functions_ga057662e8933c9837bb4bbb5ffb2822f0.html#ga057662e8933c9837bb4bbb5ffb2822f0',1,'kyosu']]],
-  ['kronecker_2',['kronecker',['../namespacekyosu_abba63e00b17a22a27b1784497338f852.html#abba63e00b17a22a27b1784497338f852',1,'kyosu']]],
-  ['kummer_3',['kummer',['../group__functions_ga83edd56ee60fedf9154fe07bad9cc580.html#ga83edd56ee60fedf9154fe07bad9cc580',1,'kyosu']]]
+  ['kolmmean_1',['kolmmean',['../group__functions_gac06cf2282bf63fa5da0684e550d7bc99.html#gac06cf2282bf63fa5da0684e550d7bc99',1,'kyosu']]],
+  ['kpart_2',['kpart',['../group__functions_ga057662e8933c9837bb4bbb5ffb2822f0.html#ga057662e8933c9837bb4bbb5ffb2822f0',1,'kyosu']]],
+  ['kronecker_3',['kronecker',['../namespacekyosu_abba63e00b17a22a27b1784497338f852.html#abba63e00b17a22a27b1784497338f852',1,'kyosu']]],
+  ['kummer_4',['kummer',['../group__functions_ga83edd56ee60fedf9154fe07bad9cc580.html#ga83edd56ee60fedf9154fe07bad9cc580',1,'kyosu']]]
 ];
