ca99cc68eb3a8a95fcb0d65c41634df78f913f04
jtlap <jtlapreste@gmail.com>
2026-01-14 14:11:01 +0100

    [no ci] spurious comment

--- include/kyosu/functions.hpp
+++ include/kyosu/functions.hpp
@@ -110,7 +110,6 @@
 #include <kyosu/functions/fsm.hpp>
 #include <kyosu/functions/gd.hpp>
 #include <kyosu/functions/gegenbauer.hpp>
-//#include <kyosu/functions/geommean.hpp>
 #include <kyosu/functions/harmmean.hpp>
 #include <kyosu/functions/hypergeometric.hpp>
 #include <kyosu/functions/hypot.hpp>
