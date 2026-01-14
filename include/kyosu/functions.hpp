10f0fc7ff51539f7087ea163ad6f150950b4f32f
jtlap <jtlapreste@gmail.com>
2026-01-14 14:19:39 +0100

    Implements means familly

--- include/kyosu/functions.hpp
+++ include/kyosu/functions.hpp
@@ -110,6 +110,7 @@
 #include <kyosu/functions/fsm.hpp>
 #include <kyosu/functions/gd.hpp>
 #include <kyosu/functions/gegenbauer.hpp>
+#include <kyosu/functions/harmmean.hpp>
 #include <kyosu/functions/hypergeometric.hpp>
 #include <kyosu/functions/hypot.hpp>
 #include <kyosu/functions/horner.hpp>
@@ -138,6 +139,7 @@
 #include <kyosu/functions/is_real.hpp>
 #include <kyosu/functions/is_unitary.hpp>
 #include <kyosu/functions/jacobi_elliptic.hpp>
+#include <kyosu/functions/kolmmean.hpp>
 #include <kyosu/functions/kronecker.hpp>
 #include <kyosu/functions/kummer.hpp>
 #include <kyosu/functions/lambda.hpp>
