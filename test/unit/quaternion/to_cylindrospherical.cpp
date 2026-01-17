10223d5df8dee8ecc387aa7a826536a04b0ff2f0
Joel Falcou <joel.falcou@lri.fr>
2026-01-17 18:35:02 +0100

    Refurbished quaternion tests

--- test/unit/quaternion/to_cylindrospherical.cpp
+++ test/unit/quaternion/to_cylindrospherical.cpp
@@ -1,41 +1,35 @@
-//==================================================================================================
-/**
-  EVE - Expressive Vector Engine
-  Copyright : EVE Project Contributors
+//======================================================================================================================
+/*
+  Kyosu - Complex Without Complexes
+  Copyright : KYOSU Contributors & Maintainers
   SPDX-License-Identifier: BSL-1.0
-**/
-//==================================================================================================
+*/
+//======================================================================================================================
 #include "test.hpp"
 #include <kyosu/kyosu.hpp>
 
-TTS_CASE_WITH ( "Check behavior of to_cylindrospherical on wide"
-              , kyosu::real_types
-              , tts::generate( tts::randoms(0.5, +1.0)
-                             , tts::randoms(0.5, +1.0)
-                             , tts::randoms(0.5, +1.0)
-                             , tts::randoms(0.5, +1.0)
-                             )
-              )
-  <typename T>(T const& a0, T const& a1, T const& a2, T const& a3 )
+TTS_CASE_WITH("Check behavior of to_cylindrospherical on wide",
+              kyosu::real_types,
+              tts::randoms(0.5, +1.0),
+              tts::randoms(0.5, +1.0),
+              tts::randoms(0.5, +1.0),
+              tts::randoms(0.5, +1.0))<typename T>(T const& a0, T const& a1, T const& a2, T const& a3){
+  {auto q = kyosu::quaternion(a0, a1, a2, a3);
+auto [r1, t1, r2, t2] = kyosu::to_cylindrospherical(q);
+auto q1 = kyosu::from_cylindrospherical(r1, t1, r2, t2);
+TTS_RELATIVE_EQUAL(q, q1, 1.0e-5);
+}
 {
-  {
-    auto  q = kyosu::quaternion(a0, a1, a2, a3);
-    auto [r1, t1, r2, t2] = kyosu::to_cylindrospherical(q);
-    auto q1 = kyosu::from_cylindrospherical(r1, t1, r2, t2);
-    TTS_RELATIVE_EQUAL(q, q1, 1.0e-5);
-  }
-  {
-    auto  c = kyosu::complex(a0, a1);
-    auto [r1, t1, r2, t2] = kyosu::to_cylindrospherical(c);
-    auto c1 = kyosu::from_cylindrospherical(r1, t1, r2, t2);
-    TTS_RELATIVE_EQUAL(kyosu::quaternion(c), c1, 1.0e-5);
-  }
+  auto c = kyosu::complex(a0, a1);
+  auto [r1, t1, r2, t2] = kyosu::to_cylindrospherical(c);
+  auto c1 = kyosu::from_cylindrospherical(r1, t1, r2, t2);
+  TTS_RELATIVE_EQUAL(kyosu::quaternion(c), c1, 1.0e-5);
+}
 
-  {
-    auto [r1, t1, r2, t2] = kyosu::to_cylindrospherical(a0);
-    auto q1 = kyosu::from_cylindrospherical(r1, t1, r2, t2);
-    TTS_RELATIVE_EQUAL(kyosu::quaternion(a0), q1, 1.0e-5);
-  }
-
-
-};
+{
+  auto [r1, t1, r2, t2] = kyosu::to_cylindrospherical(a0);
+  auto q1 = kyosu::from_cylindrospherical(r1, t1, r2, t2);
+  TTS_RELATIVE_EQUAL(kyosu::quaternion(a0), q1, 1.0e-5);
+}
+}
+;
--- test/unit/quaternion/to_euler.cpp
+++ test/unit/quaternion/to_euler.cpp
@@ -1,37 +1,30 @@
-//==================================================================================================
-/**
-  EVE - Expressive Vector Engine
-  Copyright : EVE Project Contributors
+//======================================================================================================================
+/*
+  Kyosu - Complex Without Complexes
+  Copyright : KYOSU Contributors & Maintainers
   SPDX-License-Identifier: BSL-1.0
-**/
-//==================================================================================================
+*/
+//======================================================================================================================
 #include "test.hpp"
 #include <kyosu/kyosu.hpp>
-#include <kyosu/details/decorators.hpp>
 
-
-TTS_CASE_WITH ( "Check behavior of rotate_vec on wide"
-              , kyosu::real_types
-              , tts::generate( tts::randoms(0.25, +0.75)
-                             , tts::randoms(0.25, +0.75)
-                             , tts::randoms(0.25, +0.75)
-                             )
-              )
-  <typename T>(T const& a0, T const& a1, T const& a2)
+TTS_CASE_WITH("Check behavior of rotate_vec on wide",
+              kyosu::real_types,
+              tts::randoms(0.25, +0.75),
+              tts::randoms(0.25, +0.75),
+              tts::randoms(0.25, +0.75))<typename T>(T const& a0, T const& a1, T const& a2){
+  {auto z = kyosu::from_euler(a0, a1, a2, kyosu::Z_, kyosu::X_, kyosu::Z_);
+auto [t1, t2, t3] = kyosu::to_euler(z, kyosu::Z_, kyosu::X_, kyosu::Z_);
+TTS_RELATIVE_EQUAL(t1, a0, 1.0e-4);
+TTS_RELATIVE_EQUAL(t2, a1, 1.0e-4);
+TTS_RELATIVE_EQUAL(t3, a2, 1.0e-4);
+}
 {
-  {
-    auto z = kyosu::from_euler(a0, a1, a2, kyosu::Z_, kyosu::X_, kyosu::Z_);
-    auto [t1, t2, t3] = kyosu::to_euler(z, kyosu::Z_, kyosu::X_, kyosu::Z_);
-    TTS_RELATIVE_EQUAL(t1, a0, 1.0e-4);
-    TTS_RELATIVE_EQUAL(t2, a1, 1.0e-4);
-    TTS_RELATIVE_EQUAL(t3, a2, 1.0e-4);
-  }
-  {
-    auto z = kyosu::from_euler[kyosu::intrinsic](a0, a1, a2, kyosu::Z_, kyosu::X_, kyosu::Z_);
-    auto [t1, t2, t3] = kyosu::to_euler[kyosu::intrinsic](z, kyosu::Z_, kyosu::X_, kyosu::Z_);
-    TTS_RELATIVE_EQUAL(t1, a0, 1.0e-4);
-    TTS_RELATIVE_EQUAL(t2, a1, 1.0e-4);
-    TTS_RELATIVE_EQUAL(t3, a2, 1.0e-4);
-  }
-
-};
+  auto z = kyosu::from_euler[kyosu::intrinsic](a0, a1, a2, kyosu::Z_, kyosu::X_, kyosu::Z_);
+  auto [t1, t2, t3] = kyosu::to_euler[kyosu::intrinsic](z, kyosu::Z_, kyosu::X_, kyosu::Z_);
+  TTS_RELATIVE_EQUAL(t1, a0, 1.0e-4);
+  TTS_RELATIVE_EQUAL(t2, a1, 1.0e-4);
+  TTS_RELATIVE_EQUAL(t3, a2, 1.0e-4);
+}
+}
+;
