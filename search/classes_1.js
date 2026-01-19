2883afe045b78b8f3f9fbbded1944db7d2557c89
jfalcou <jfalcou@users.noreply.github.com>
2026-01-19 15:54:14 +0000

    deploy: 9620c7b1c568ea88d470d0b5cc5a2ad5344a78a9

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['cayley_5fdickson_0',['cayley_dickson',['../structkyosu_1_1cayley__dickson.html',1,'kyosu']]],
-  ['complexify_1',['complexify',['../structkyosu_1_1complexify.html',1,'kyosu']]]
+  ['complexify_1',['complexify',['../structkyosu_1_1complexify.html',1,'kyosu']]],
+  ['complexify_5fif_2',['complexify_if',['../structkyosu_1_1complexify__if.html',1,'kyosu']]]
 ];
