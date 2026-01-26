ea16a9eee7f351e6c7a951ca7dfdeaec8dc47cbb
jtlap <jtlapreste@gmail.com>
2026-01-26 20:16:10 +0100

    functions

--- include/kyosu/functions.hpp
+++ include/kyosu/functions.hpp
@@ -109,10 +109,7 @@
 #include <kyosu/functions/fsm.hpp>
 #include <kyosu/functions/gd.hpp>
 #include <kyosu/functions/gegenbauer.hpp>
-<<<<<<< Updated upstream
-=======
 #include <kyosu/functions/geommean.hpp>
->>>>>>> Stashed changes
 #include <kyosu/functions/harmmean.hpp>
 #include <kyosu/functions/hypergeometric.hpp>
 #include <kyosu/functions/hypot.hpp>
