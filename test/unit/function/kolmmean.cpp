84d3544e0a6917a0fd008cf4ac6da65b375895eb
jtlap <jtlapreste@gmail.com>
2026-02-03 22:21:33 +0100

    k*.cpp

--- test/unit/function/kolmmean.cpp
+++ test/unit/function/kolmmean.cpp
@@ -8,39 +8,38 @@
 #include <kyosu/kyosu.hpp>
 #include <test.hpp>
 
-TTS_CASE_WITH ( "Check kyosu::kolmmean over real"
-              , kyosu::real_types
-              , tts::generate(tts::randoms(0,10)
-                             ,tts::randoms(0,10)
-                             )
-              )
-  <typename T>(T r0, T r1)
+TTS_CASE_WITH("Check kyosu::kolmmean over real", kyosu::real_types, tts::randoms(0, 10), tts::randoms(0, 10)
+
+)
+<typename T>(T r0, T r1)
 {
   // quadratic mean
-  auto f = [](auto x){return kyosu::sqr(x); };
-  auto g = [](auto x){return eve::sqrt(x); };
+  auto f = [](auto x) { return kyosu::sqr(x); };
+  auto g = [](auto x) { return eve::sqrt(x); };
   auto pr = tts::prec<T>(1.0e-1, 1.0e-6);
   TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, r0), r0, pr);
-  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, r0, r1), eve::hypot(r0, r1)/eve::sqrt_2(eve::as(r0)), pr);
-  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, r0, r1, r1), eve::hypot(r0, r1, r1)/eve::sqrt_3(eve::as(r0)), pr);
+  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, r0, r1), eve::hypot(r0, r1) / eve::sqrt_2(eve::as(r0)), pr);
+  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, r0, r1, r1), eve::hypot(r0, r1, r1) / eve::sqrt_3(eve::as(r0)), pr);
 };
 
-TTS_CASE_WITH ( "Check kyosu::kolmmean over complex"
-              , kyosu::real_types
-              , tts::generate(tts::randoms(-10,10), tts::randoms(-10,10)
-                             ,tts::randoms(-10,10), tts::randoms(-10,10)
-                             )
-              )
+TTS_CASE_WITH("Check kyosu::kolmmean over complex",
+              kyosu::real_types,
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10)
+
+)
 <typename T>(T r0, T i0, T r1, T i1)
 {
   auto pr = tts::prec<T>(1.0e-1, 1.0e-6);
-  auto c0 = kyosu::complex(r0,i0);
-  auto c1 = kyosu::complex(r1,i1);
-  auto f = [](auto x){return kyosu::sqr(x); };
-  auto g = [](auto x){return kyosu::sqrt(x); };
+  auto c0 = kyosu::complex(r0, i0);
+  auto c1 = kyosu::complex(r1, i1);
+  auto f = [](auto x) { return kyosu::sqr(x); };
+  auto g = [](auto x) { return kyosu::sqrt(x); };
   TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, c0), c0, pr);
 
-  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, c0, c1), g(f(c0)+f( c1))/eve::sqrt_2(eve::as(r0)), pr);
-  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, c0, c1, r1), g(f(c0)+f(c1)+f(r1))/eve::sqrt_3(eve::as(r0)), pr);
-  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, r0, c1, r1), g(f(r0)+f(c1)+f(r1))/eve::sqrt_3(eve::as(r0)), pr);
+  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, c0, c1), g(f(c0) + f(c1)) / eve::sqrt_2(eve::as(r0)), pr);
+  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, c0, c1, r1), g(f(c0) + f(c1) + f(r1)) / eve::sqrt_3(eve::as(r0)), pr);
+  TTS_RELATIVE_EQUAL(kyosu::kolmmean(f, g, r0, c1, r1), g(f(r0) + f(c1) + f(r1)) / eve::sqrt_3(eve::as(r0)), pr);
 };
--- test/unit/function/kronecker.cpp
+++ test/unit/function/kronecker.cpp
@@ -8,12 +8,9 @@
 #include <kyosu/kyosu.hpp>
 #include <test.hpp>
 
-TTS_CASE_WITH ( "Check kyosu::kronecker over real"
-              , kyosu::real_types
-              , tts::generate(tts::randoms(-10,10)
-                             ,tts::randoms(-10,10)
-                             )
-              )
+TTS_CASE_WITH("Check kyosu::kronecker over real", kyosu::real_types, tts::randoms(-10, 10), tts::randoms(-10, 10)
+
+)
 (auto r0, auto r1)
 {
   TTS_EQUAL(kyosu::kronecker(r0, r1), kyosu::if_else(r0 == r1, kyosu::one(kyosu::as(r0)), kyosu::zero));
@@ -21,34 +18,40 @@ TTS_CASE_WITH ( "Check kyosu::kronecker over real"
   TTS_EQUAL(kyosu::kronecker(r1, r1, r1), kyosu::one(kyosu::as(r1)));
 };
 
-TTS_CASE_WITH ( "Check kyosu::kronecker over complex"
-              , kyosu::real_types
-              , tts::generate(tts::randoms(-10,10), tts::randoms(-10,10)
-                             ,tts::randoms(-10,10), tts::randoms(-10,10)
-                             )
-              )
+TTS_CASE_WITH("Check kyosu::kronecker over complex",
+              kyosu::real_types,
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10)
+
+)
 <typename T>(T r0, T i0, T r1, T i1)
 {
-  auto c0 = kyosu::complex(r0,i0);
-  auto c1 = kyosu::complex(r1,i1);
+  auto c0 = kyosu::complex(r0, i0);
+  auto c1 = kyosu::complex(r1, i1);
   TTS_EQUAL(kyosu::kronecker(c0, c1), kyosu::if_else(c0 == c1, kyosu::one(kyosu::as(r0)), kyosu::zero));
   TTS_EQUAL(kyosu::kronecker(c0, c1, r1), kyosu::zero(kyosu::as(r0)));
   TTS_EQUAL(kyosu::kronecker(c1, c1, c1), kyosu::one(kyosu::as(r0)));
 };
 
-TTS_CASE_WITH ( "Check kyosu::kronecker over quaternion"
-              , kyosu::real_types
-              , tts::generate ( tts::randoms(-10,10), tts::randoms(-10,10)
-                              , tts::randoms(-10,10), tts::randoms(-10,10)
-                              , tts::randoms(-10,10), tts::randoms(-10,10)
-                              , tts::randoms(-10,10), tts::randoms(-10,10)
-                              )
-              )
+TTS_CASE_WITH("Check kyosu::kronecker over quaternion",
+              kyosu::real_types,
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10)
+
+)
 <typename T>(T r0, T i0, T j0, T k0, T r1, T i1, T j1, T k1)
 {
   using type = kyosu::quaternion_t<T>;
-  auto q0 = type(r0,i0,j0,k0);
-  auto q1 = type(r1,i1,j1,k1);
+  auto q0 = type(r0, i0, j0, k0);
+  auto q1 = type(r1, i1, j1, k1);
   TTS_EQUAL(kyosu::kronecker(q0, q1), kyosu::if_else(q0 == q1, kyosu::one(kyosu::as(r0)), kyosu::zero));
   TTS_EQUAL(kyosu::kronecker(q0, q1, r1), kyosu::zero(kyosu::as(r0)));
   TTS_EQUAL(kyosu::kronecker(q1, q1, q1), kyosu::one(kyosu::as(r0)));
