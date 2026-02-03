4e50277cb36c934fc9adab89199daf4b063d927c
jtlap <jtlapreste@gmail.com>
2026-02-03 21:25:40 +0100

    e*.cpp

--- test/unit/function/exp_ipi.cpp
+++ test/unit/function/exp_ipi.cpp
@@ -8,22 +8,24 @@
 #include <kyosu/kyosu.hpp>
 #include <test.hpp>
 
-TTS_CASE_WITH ( "Check kyosu::exp_ipi over quaternion"
-              , kyosu::real_types
-              , tts::generate ( tts::randoms(0.5,2.0), tts::randoms(0.5,2.0)
-                              , tts::randoms(0.5,2.0), tts::randoms(0.5,2.0)
-                              )
-              )
+TTS_CASE_WITH("Check kyosu::exp_ipi over quaternion",
+              kyosu::real_types,
+              tts::randoms(0.5, 2.0),
+              tts::randoms(0.5, 2.0),
+              tts::randoms(0.5, 2.0),
+              tts::randoms(0.5, 2.0)
+
+)
 <typename T>(T r, T i, T j, T k)
 {
   using u_t = eve::underlying_type_t<T>;
   auto ipi = kyosu::complex_t<u_t>(u_t(0), eve::pi(eve::as<u_t>()));
 
-  TTS_RELATIVE_EQUAL(kyosu::exp_ipi(r), kyosu::exp(ipi*r), tts::prec<T>());
+  TTS_RELATIVE_EQUAL(kyosu::exp_ipi(r), kyosu::exp(ipi * r), tts::prec<T>());
   using ce_t = kyosu::complex_t<T>;
-  auto c = ce_t(r,i);
-  TTS_RELATIVE_EQUAL(kyosu::exp_ipi(c), kyosu::exp(ipi*c), tts::prec<T>());
+  auto c = ce_t(r, i);
+  TTS_RELATIVE_EQUAL(kyosu::exp_ipi(c), kyosu::exp(ipi * c), tts::prec<T>());
   using qe_t = kyosu::quaternion_t<T>;
-  auto q = qe_t(r,i,j,k);
-  TTS_RELATIVE_EQUAL(kyosu::exp_ipi(q), kyosu::exp(ipi*q), tts::prec<T>());
+  auto q = qe_t(r, i, j, k);
+  TTS_RELATIVE_EQUAL(kyosu::exp_ipi(q), kyosu::exp(ipi * q), tts::prec<T>());
 };
--- test/unit/function/expx2.cpp
+++ test/unit/function/expx2.cpp
@@ -8,19 +8,21 @@
 #include <kyosu/kyosu.hpp>
 #include <test.hpp>
 
-TTS_CASE_WITH ( "Check kyosu::expx2 over quaternion"
-              , kyosu::real_types
-              , tts::generate ( tts::randoms(0.5,2.0), tts::randoms(0.5,2.0)
-                              , tts::randoms(0.5,2.0), tts::randoms(0.5,2.0)
-                              )
-              )
+TTS_CASE_WITH("Check kyosu::expx2 over quaternion",
+              kyosu::real_types,
+              tts::randoms(0.5, 2.0),
+              tts::randoms(0.5, 2.0),
+              tts::randoms(0.5, 2.0),
+              tts::randoms(0.5, 2.0)
+
+)
 <typename T>(T r, T i, T j, T k)
 {
   TTS_RELATIVE_EQUAL(kyosu::expx2(r), kyosu::exp(kyosu::sqr(r)), tts::prec<T>());
   using ce_t = kyosu::complex_t<T>;
-  auto c = ce_t(r,i);
+  auto c = ce_t(r, i);
   TTS_RELATIVE_EQUAL(kyosu::expx2(c), kyosu::exp(kyosu::sqr(c)), tts::prec<T>());
   using qe_t = kyosu::quaternion_t<T>;
-  auto q = qe_t(r,i,j,k);
+  auto q = qe_t(r, i, j, k);
   TTS_RELATIVE_EQUAL(kyosu::expx2(q), kyosu::exp(kyosu::sqr(q)), tts::prec<T>());
 };
