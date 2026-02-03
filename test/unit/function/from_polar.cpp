35e619d60918116f78d23e18cddf25b11c78415d
jtlap <jtlapreste@gmail.com>
2026-02-03 21:30:26 +0100

    f*.cpp

--- test/unit/function/from_polar.cpp
+++ test/unit/function/from_polar.cpp
@@ -8,17 +8,20 @@
 #include <kyosu/kyosu.hpp>
 #include <test.hpp>
 
-TTS_CASE_WITH ( "Check kyosu::from_polar over real"
-              , kyosu::real_types
-              , tts::generate( tts::randoms(0,10) ,tts::randoms(-10,10)
-                             , tts::randoms(-10,10), tts::randoms(-10,10)
-                             , tts::randoms(-10,10), tts::randoms(-10,10)
-                             )
-              )
+TTS_CASE_WITH("Check kyosu::from_polar over real",
+              kyosu::real_types,
+              tts::randoms(0, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10)
+
+)
 (auto rho, auto theta, auto r, auto i, auto j, auto k)
 {
-  using T =  decltype(rho);
-  auto iq = kyosu::sign(kyosu::quaternion(r,i,j,k));
- TTS_RELATIVE_EQUAL(kyosu::from_polar(rho, theta, iq), rho*kyosu::exp(theta*iq), tts::prec<T>());
-  TTS_RELATIVE_EQUAL(kyosu::from_polar(rho, theta),     rho*kyosu::exp_i(theta),  tts::prec<T>());
+  using T = decltype(rho);
+  auto iq = kyosu::sign(kyosu::quaternion(r, i, j, k));
+  TTS_RELATIVE_EQUAL(kyosu::from_polar(rho, theta, iq), rho * kyosu::exp(theta * iq), tts::prec<T>());
+  TTS_RELATIVE_EQUAL(kyosu::from_polar(rho, theta), rho * kyosu::exp_i(theta), tts::prec<T>());
 };
