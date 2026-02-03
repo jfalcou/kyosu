711de523cca9c84f8eca210503d30ad0e8922a36
jtlap <jtlapreste@gmail.com>
2026-02-03 22:36:40 +0100

    r*.cpp

--- test/unit/function/rec.cpp
+++ test/unit/function/rec.cpp
@@ -8,26 +8,24 @@
 #include <kyosu/kyosu.hpp>
 #include <test.hpp>
 
-TTS_CASE_WITH ( "Check kyosu::rec over real"
-              , kyosu::real_types
-              , tts::generate(tts::randoms(-10,10))
-              )
+TTS_CASE_WITH("Check kyosu::rec over real", kyosu::real_types, tts::randoms(-10, 10))
 (auto data)
 {
   TTS_ULP_EQUAL(kyosu::rec(data), eve::rec(data), 0.5);
 };
 
+TTS_CASE_WITH("Check kyosu::rec over quaternion",
+              kyosu::simd_real_types,
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10)
 
-TTS_CASE_WITH ( "Check kyosu::rec over quaternion"
-              , kyosu::simd_real_types
-              , tts::generate ( tts::randoms(-10,10), tts::randoms(-10,10)
-                              , tts::randoms(-10,10), tts::randoms(-10,10)
-                              )
-              )
+)
 <typename T>(T r, T i, T j, T k)
 {
   auto o = eve::one(eve::as(eve::underlying_type_t<T>()));
   using ke_t = kyosu::quaternion_t<T>;
-  auto q = ke_t(r,i,j,k);
-  TTS_RELATIVE_EQUAL(kyosu::rec(q), o/q, tts::prec<T>());
+  auto q = ke_t(r, i, j, k);
+  TTS_RELATIVE_EQUAL(kyosu::rec(q), o / q, tts::prec<T>());
 };
--- test/unit/function/rsqrt.cpp
+++ test/unit/function/rsqrt.cpp
@@ -8,49 +8,46 @@
 #include <kyosu/kyosu.hpp>
 #include <test.hpp>
 
-TTS_CASE_WITH ( "Check kyosu::rsqrt over real"
-              , kyosu::real_types
-              , tts::generate(tts::randoms(-10,10))
-              )
-  <typename T>  (T v)
+TTS_CASE_WITH("Check kyosu::rsqrt over real", kyosu::real_types, tts::randoms(-10, 10))
+<typename T>(T v)
 {
   using ce_t = kyosu::complex_t<T>;
   auto asq = eve::rsqrt(eve::abs(v));
-  TTS_RELATIVE_EQUAL(kyosu::rsqrt(v),  kyosu::if_else(eve::is_gez(v)
-                                                    , kyosu::complex(asq, T(0))
-                                                     , kyosu::complex(T(0), eve::signnz(v)*asq)), tts::prec<T>());
+  TTS_RELATIVE_EQUAL(
+    kyosu::rsqrt(v),
+    kyosu::if_else(eve::is_gez(v), kyosu::complex(asq, T(0)), kyosu::complex(T(0), eve::signnz(v) * asq)),
+    tts::prec<T>());
   auto rr = eve::rsqrt(v);
   auto re = kyosu::rsqrt[kyosu::real_only](v);
   TTS_IEEE_EQUAL(re, rr);
   auto cond = eve::is_ltz(v);
 
-  TTS_RELATIVE_EQUAL(kyosu::rsqrt[cond][kyosu::real_only](v), kyosu::if_else(cond,  eve::rsqrt(v), v), tts::prec<T>());
-  TTS_RELATIVE_EQUAL(kyosu::rsqrt[cond](v), kyosu::if_else(cond,  kyosu::rsqrt(v), ce_t(v)), tts::prec<T>());
+  TTS_RELATIVE_EQUAL(kyosu::rsqrt[cond][kyosu::real_only](v), kyosu::if_else(cond, eve::rsqrt(v), v), tts::prec<T>());
+  TTS_RELATIVE_EQUAL(kyosu::rsqrt[cond](v), kyosu::if_else(cond, kyosu::rsqrt(v), ce_t(v)), tts::prec<T>());
 };
 
-TTS_CASE_WITH ( "Check kyosu::rsqrt over complex"
-              , kyosu::real_types
-              , tts::generate(tts::randoms(-10,10), tts::randoms(-10,10))
-              )
-  <typename T>(T r, T i)
+TTS_CASE_WITH("Check kyosu::rsqrt over complex", kyosu::real_types, tts::randoms(-10, 10), tts::randoms(-10, 10))
+<typename T>(T r, T i)
 {
-  auto c = kyosu::complex(r,i);
+  auto c = kyosu::complex(r, i);
   TTS_RELATIVE_EQUAL(kyosu::rec(kyosu::sqr(kyosu::rsqrt(c))), c, tts::prec<T>());
   auto cond = eve::is_ltz(r);
-  TTS_RELATIVE_EQUAL(kyosu::rsqrt[cond](c), kyosu::if_else(cond,  kyosu::rsqrt(c), c), tts::prec<T>());
+  TTS_RELATIVE_EQUAL(kyosu::rsqrt[cond](c), kyosu::if_else(cond, kyosu::rsqrt(c), c), tts::prec<T>());
 };
 
-TTS_CASE_WITH ( "Check kyosu::rsqrt over quaternion"
-              , kyosu::real_types
-              , tts::generate ( tts::randoms(-10,10), tts::randoms(-10,10)
-                              , tts::randoms(-10,10), tts::randoms(-10,10)
-                              )
-              )
-  <typename T>(T r, T i, T j, T k)
+TTS_CASE_WITH("Check kyosu::rsqrt over quaternion",
+              kyosu::real_types,
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10)
+
+)
+<typename T>(T r, T i, T j, T k)
 {
   using type = kyosu::quaternion_t<T>;
-  auto q = type(r,i,j,k);
+  auto q = type(r, i, j, k);
   TTS_RELATIVE_EQUAL(kyosu::rec(kyosu::sqr((kyosu::rsqrt(q)))), q, tts::prec<T>());
   auto cond = eve::is_ltz(r);
-  TTS_RELATIVE_EQUAL(kyosu::rsqrt[cond](q), kyosu::if_else(cond,  kyosu::rsqrt(q), q), tts::prec<T>());
+  TTS_RELATIVE_EQUAL(kyosu::rsqrt[cond](q), kyosu::if_else(cond, kyosu::rsqrt(q), q), tts::prec<T>());
 };
