ce0b66ad527b09129d64bd6b166ab1c8e382bdfa
jtlap <jtlapreste@gmail.com>
2026-03-02 14:28:20 +0100

    cb_ikr

--- include/kyosu/details/bessel/besselr/cb_ikr.hpp
+++ include/kyosu/details/bessel/besselr/cb_ikr.hpp
@@ -23,22 +23,18 @@ namespace kyosu::_
   //  cb_kr just last
   //===-------------------------------------------------------------------------------------------
 
-  template<eve::floating_scalar_value N, typename Z>
-  auto cb_kr(N v, Z z) noexcept
+  template<eve::floating_scalar_value N, typename Z> auto cb_kr(N v, Z z) noexcept
   {
     v = eve::abs(v); //K(-v, z) == K(v, z) DLMF 10.27.1
     using u_t = eve::underlying_type_t<Z>;
     auto argz = arg(z);
     auto piotwo = eve::pio_2(eve::as<u_t>());
     auto i = complex(u_t(0), u_t(1));
-    auto f = muli(exp_ipi(v/2));
-    auto cpi = piotwo*f;
-    auto cmi = piotwo*rec(f);
+    auto f = muli(exp_ipi(v / 2));
+    auto cpi = piotwo * f;
+    auto cmi = piotwo * rec(f);
     auto argzlt0 = eve::is_ltz(argz);
-    auto r =  if_else(argzlt0
-                     , cpi*cb_h1r(v, muli(z))
-                     , cmi*cb_h2r(v, mulmi(z))
-                     );
+    auto r = if_else(argzlt0, cpi * cb_h1r(v, muli(z)), cmi * cb_h2r(v, mulmi(z)));
     return if_else(is_eqz(z), complex(eve::inf(eve::as<u_t>())), r);
   }
 
@@ -46,44 +42,42 @@ namespace kyosu::_
   //  cb_kr all possible outputs
   //===-------------------------------------------------------------------------------------------
 
-  template<eve::floating_scalar_value N, typename Z, typename R>
-  auto cb_kr(N v, Z z, R& ks) noexcept
+  template<eve::floating_scalar_value N, typename Z, typename R> auto cb_kr(N v, Z z, R& ks) noexcept
   {
-    v = eve::abs(v);  //k(-v, z) == K(v, z) DLMF 10.27.1
+    v = eve::abs(v); //k(-v, z) == K(v, z) DLMF 10.27.1
     using u_t = eve::underlying_type_t<Z>;
     auto n = std::size_t(v); //n>= 0
-    auto v0 = v-n;
+    auto v0 = v - n;
     auto vi = v0;
-    for(int j=0; j < eve::min(size_t(n+1), size(ks)); ++j)
+    for (int jj = 0; jj < eve::min(size_t(n + 1), size(ks)); ++jj)
     {
-      ks[j] = cb_kr(vi, z);
+      ks[jj] = cb_kr(vi, z);
       vi = eve::inc(vi);
     }
-    return cb_kr(v0+n, z);
+    return cb_kr(v0 + n, z);
   }
 
- //===-------------------------------------------------------------------------------------------
+  //===-------------------------------------------------------------------------------------------
   //  cb_ir n outputs
   //===-------------------------------------------------------------------------------------------
-  template<eve::floating_scalar_value N, typename Z, typename R>
-  Z cb_ir(N v, Z z, R& cis) noexcept
+  template<eve::floating_scalar_value N, typename Z, typename R> Z cb_ir(N v, Z z, R& cis) noexcept
   {
     size_t an = eve::abs(int(v));
-    auto doit =  [an, v, &z, &cis](auto is){
+    auto doit = [an, v, &z, &cis](auto is) {
       //DLMF 10.27.6 with conjugation for arg(z) > 0
       if (v > 0)
       {
         auto argzpos = eve::is_gtz(kyosu::arg(z));
         z = if_else(argzpos, conj(z), z);
         cb_jr(v, muli(z), is);
-        auto v0 = v-an;
+        auto v0 = v - an;
         auto vi = v0;
-        for(int i=0; i <= an ; ++i)
+        for (int ii = 0; ii <= an; ++ii)
         {
-          auto fac = exp_ipi(-vi/2);
-          vi =  inc(vi);
-          is[i]*= fac;
-          is[i] = if_else(argzpos, conj(is[i]), is[i]);
+          auto fac = exp_ipi(-vi / 2);
+          vi = inc(vi);
+          is[ii] *= fac;
+          is[ii] = if_else(argzpos, conj(is[ii]), is[ii]);
         }
       }
       else
@@ -91,45 +85,44 @@ namespace kyosu::_
         cb_ir(-v, z, is);
         auto mvi = frac(-v);
         auto spv = eve::sinpi(mvi);
-        for(size_t l=0; l <= an; ++l)
+        for (size_t ll = 0; ll <= an; ++ll)
         {
-          is[l] += kyosu::two_o_pi(as(z))*spv*cb_kr(mvi, z);
+          is[ll] += kyosu::two_o_pi(as(z)) * spv * cb_kr(mvi, z);
           mvi = inc(mvi);
           spv = -spv;
         }
       }
-      for(int i=0; i < eve::min(size(cis), an+1); ++i) cis[i] = is[i];
+      for (int ii = 0; ii < eve::min(size(cis), an + 1); ++ii) cis[ii] = is[ii];
       return is[an];
     };
-    return with_alloca<Z>(an+1, doit);
+    return with_alloca<Z>(an + 1, doit);
   }
 
   //===-------------------------------------------------------------------------------------------
   //  cb_ir just last
   //===-------------------------------------------------------------------------------------------
-  template<eve::floating_scalar_value N, typename Z>
-  auto cb_ir(N v, Z z) noexcept
+  template<eve::floating_scalar_value N, typename Z> auto cb_ir(N v, Z z) noexcept
   {
     if (v >= 0)
     {
       auto argzpos = eve::is_gtz(kyosu::arg(z));
       z = if_else(argzpos, conj(z), z);
-      auto fac = exp_ipi(-v/2);
-      auto r = fac*_::cb_jr(v,kyosu::muli(z));
+      auto fac = exp_ipi(-v / 2);
+      auto r = fac * _::cb_jr(v, kyosu::muli(z));
       return if_else(argzpos, conj(r), r);
     }
     else
     {
       auto mv = -v;
-      return cb_ir(mv, z)+kyosu::two_o_pi(as(z))*sinpi(mv)*cb_kr(mv, z);
+      return cb_ir(mv, z) + kyosu::two_o_pi(as(z)) * sinpi(mv) * cb_kr(mv, z);
     }
   }
 
   //===-------------------------------------------------------------------------------------------
   //  cb_ik
   //===-------------------------------------------------------------------------------------------
-  template<eve::floating_scalar_value N, typename Z, typename R1, typename R2> KYOSU_FORCEINLINE
-  auto cb_ik(N v, Z z, R1& is, R2& ks) noexcept
+  template<eve::floating_scalar_value N, typename Z, typename R1, typename R2>
+  KYOSU_FORCEINLINE auto cb_ik(N v, Z z, R1& is, R2& ks) noexcept
   {
     auto in = cb_ir(v, z, is);
     auto kn = cb_kr(v, z, ks);
