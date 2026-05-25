0f3dfea0882566976c2245c2b741fcac627b9925
jtlap <jtlapreste@gmail.com>
2026-05-25 19:57:51 +0200

    sqr_abs ok tested

---
+++ test/unit/complex/sqr_abs.cpp
@@ -0,0 +1,43 @@
+//======================================================================================================================
+/*
+  Kyosu - Complex Without Complexes
+  Copyright : KYOSU Contributors & Maintainers
+  SPDX-License-Identifier: BSL-1.0
+*/
+//======================================================================================================================
+#include <kyosu/kyosu.hpp>
+#include <test.hpp>
+#include <complex>
+
+template<typename T> auto cv(std::complex<T> const& sc)
+{
+  return kyosu::complex(sc.real(), sc.imag());
+}
+
+TTS_CASE_WITH("Check behavior of sqr_abs on scalar",
+              tts::bunch<kyosu::scalar_real_types>,
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10))
+<typename T>(T const& a0, T const& a1)
+{
+  using e_t = typename T::value_type;
+  using c_t = std::complex<e_t>;
+  using kc_t = kyosu::complex_t<e_t>;
+  for (size_t i = 0; i < a0.size(); ++i)
+  {
+    auto e = a0[i];
+    auto f = a1[i];
+
+    TTS_RELATIVE_EQUAL(kyosu::sqr_abs(kc_t(e, f)), std::norm(c_t(e, f)), tts::prec<T>());
+  }
+};
+
+TTS_CASE_WITH("Check behavior of sqrt on wide", kyosu::simd_real_types, tts::randoms(-10, 10), tts::randoms(-10, 10))
+<typename T>(T const& a0, T const& a1)
+{
+  using e_t = T;
+  using ke_t = kyosu::complex_t<e_t>;
+  using c_t = std::complex<eve::element_type_t<e_t>>;
+  T e([&](auto i, auto) { return std::norm(c_t(a0.get(i), a1.get(i))); });
+  TTS_RELATIVE_EQUAL(kyosu::sqr_abs(ke_t{a0, a1}), e, tts::prec<T>());
+};
---
+++ test/unit/function/sqr_abs.cpp
@@ -0,0 +1,52 @@
+//======================================================================================================================
+/*
+  Kyosu - Complex Without Complexes
+  Copyright : KYOSU Contributors & Maintainers
+  SPDX-License-Identifier: BSL-1.0
+*/
+//======================================================================================================================
+#include <kyosu/kyosu.hpp>
+#include <test.hpp>
+
+TTS_CASE_WITH("Check kyosu::sqr_abs over real", kyosu::real_types, tts::randoms(-10, 10))
+(auto data)
+{
+  TTS_ULP_EQUAL(kyosu::sqr_abs(data), eve::sqr(data), 0.5);
+};
+
+TTS_CASE_WITH("Check kyosu::sqr_abs over complex", kyosu::real_types, tts::randoms(-10, 10), tts::randoms(-10, 10))
+(auto r, auto i)
+{
+  TTS_ULP_EQUAL(kyosu::sqr_abs(kyosu::complex(r, i)), eve::sqr(eve::hypot[eve::pedantic](r, i)), 0.5);
+};
+
+TTS_CASE_WITH("Check kyosu::abs over quaternion",
+              kyosu::real_types,
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10)
+
+)
+(auto r, auto i, auto j, auto k)
+{
+  TTS_ULP_EQUAL(kyosu::sqr_abs(kyosu::quaternion(r, i, j, k)), eve::sqr(eve::hypot[eve::pedantic](r, i, j, k)), 1.0);
+};
+
+TTS_CASE_WITH("Check kyosu::abs over octonion",
+              kyosu::real_types,
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10),
+              tts::randoms(-10, 10)
+
+)
+<typename T>(T r, T i, T j, T k, T l, T li, T lj, T lk)
+{
+  TTS_ULP_EQUAL(kyosu::sqr_abs(kyosu::octonion_t<T>(r, i, j, k, l, li, lj, lk)),
+                eve::sqr(eve::hypot[eve::pedantic](r, i, j, k, l, li, lj, lk)), 1.0);
+};
