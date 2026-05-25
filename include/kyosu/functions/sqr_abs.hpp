26a5cb5cd1e94d09369a8d52fe6f19dc2bc8508f
jtlap <jtlapreste@gmail.com>
2026-05-25 22:47:11 +0200

    Sqr family improvement

--- include/kyosu/functions/sqr_abs.hpp
+++ include/kyosu/functions/sqr_abs.hpp
@@ -11,7 +11,7 @@
 
 namespace kyosu
 {
-  template<typename Options> struct sqr_abs_t : eve::elementwise_callable<sqr_abs_t, Options>
+  template<typename Options> struct sqr_abs_t : eve::elementwise_callable<sqr_abs_t, Options, pedantic_option>
   {
     template<concepts::cayley_dickson_like Z>
     KYOSU_FORCEINLINE constexpr as_real_type_t<Z> operator()(Z z) const noexcept
@@ -39,7 +39,8 @@ namespace kyosu
   //!   @code
   //!   namespace kyosu
   //!   {
-  //!      template<kyosu::concepts::cayley_dickson_like T> constexpr as_real_type_t<T>  sqr_abs(T z) noexcept;
+  //!      template<kyosu::concepts::cayley_dickson_like T> constexpr as_real_type_t<T>  sqr_abs(T z)           noexcept; //1
+  //!      template<kyosu::concepts::cayley_dickson_like T> constexpr as_real_type_t<T>  sqr_abs[pedantic](T z) noexcept; //2
   //!   }
   //!   @endcode
   //!
@@ -49,7 +50,8 @@ namespace kyosu
   //!
   //!   **Return value**
   //!
-  //!     Returns the squared modulus of its argument, a floating value.
+  //!     1. Returns the squared modulus of its argument, a floating value.
+  //!     2. Returns inf as soon as the sum of the non-nan squared components is infinite
   //!
   //!  @groupheader{Example}
   //!
@@ -67,12 +69,40 @@ namespace kyosu::_
   KYOSU_FORCEINLINE constexpr auto sqr_abs_(KYOSU_DELAY(), O const&, Z v) noexcept
   {
     if constexpr (concepts::real<Z>) return eve::sqr(v);
+    else if constexpr (O::contains(pedantic))
+    {
+      if constexpr (concepts::complex<Z>)
+      {
+        auto [r, i] = v;
+        auto r2 = eve::sqr(r);
+        auto i2 = eve::sqr(i);
+        return eve::if_else(eve::is_infinite(r2) || eve::is_infinite(i2), eve::inf(as(r)), i2 + r2);
+      }
+      else
+      {
+        auto nanfound = eve::false_(as<Z>());
+        auto squares = kumi::map(
+          [&nanfound](auto const& e) {
+            auto nanfound = eve::is_nan(e);
+            return if_else(nanfound, eve::zero, eve::sqr(e));
+          },
+          v);
+        auto r = kumi::sum(kumi::extract(squares, kumi::index<1>), get<0>(squares));
+        return if_else(is_infinite(r), eve::inf(as(r)), if_else(nanfound, eve::allbits, r));
+      }
+    }
     else
     {
-      auto anyinf = kumi::any_of(v, eve::is_infinite);
-      auto squares = kumi::map([](auto const& e) { return e * e; }, v);
-      auto r = kumi::sum(kumi::extract(squares, kumi::index<1>), get<0>(squares));
-      return if_else(anyinf, eve::inf(as(r)), r);
+      if constexpr (concepts::complex<Z>)
+      {
+        auto [r, i] = v;
+        return eve::sqr(r) + eve::sqr(i);
+      }
+      else
+      {
+        auto squares = kumi::map([](auto const& e) { return eve::sqr(e); }, v);
+        return kumi::sum(kumi::extract(squares, kumi::index<1>), get<0>(squares));
+      }
     }
   }
 }
