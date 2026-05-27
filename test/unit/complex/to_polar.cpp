da07cac999169e6ccd3883a4ce9a574f23508a56
jtlap <jtlapreste@gmail.com>
2026-05-27 11:46:12 +0200

    ulps

--- test/unit/complex/to_polar.cpp
+++ test/unit/complex/to_polar.cpp
@@ -20,34 +20,34 @@ TTS_CASE_TPL("Check to_complex converter from constants", kyosu::scalar_real_typ
   auto wo(w_t(1));
 
   auto [r0, t0] = kyosu::to_polar(zer);
-  TTS_EQUAL(r0, T{0});
-  TTS_EQUAL(t0, T{0});
+  TTS_ULP_EQUAL(r0, T{0}, 0.5);
+  TTS_ULP_EQUAL(t0, T{0}, 0.5);
 
   auto [wr0, wt0] = kyosu::to_polar(wzer);
-  TTS_EQUAL(wr0, w_t{0});
-  TTS_EQUAL(wt0, w_t{0});
+  TTS_ULP_EQUAL(wr0, w_t{0}, 0.5);
+  TTS_ULP_EQUAL(wt0, w_t{0}, 0.5);
 
   {
     auto [r_1, t_1] = kyosu::to_polar(o, o);
 
-    TTS_EQUAL(r_1, eve::sqrt_2(eve::as(r_1)));
-    TTS_EQUAL(t_1, eve::pio_4(eve::as(t_1)));
+    TTS_ULP_EQUAL(r_1, eve::sqrt_2(eve::as(r_1)), 0.5);
+    TTS_ULP_EQUAL(t_1, eve::pio_4(eve::as(t_1)), 0.5);
 
     auto [rc, tc] = kyosu::to_polar(c_t(1, 1));
 
-    TTS_EQUAL(rc, eve::sqrt_2(eve::as(rc)));
-    TTS_EQUAL(tc, eve::pio_4(eve::as(tc)));
+    TTS_ULP_EQUAL(rc, eve::sqrt_2(eve::as(rc)), 0.5);
+    TTS_ULP_EQUAL(tc, eve::pio_4(eve::as(tc)), 0.5);
 
     auto [wr_1, wt_1] = kyosu::to_polar(wo, wo);
-    TTS_EQUAL(wr_1, eve::sqrt_2(eve::as(wr_1)));
-    TTS_EQUAL(wt_1, eve::pio_4(eve::as(wt_1)));
+    TTS_ULP_EQUAL(wr_1, eve::sqrt_2(eve::as(wr_1)), 0.5);
+    TTS_ULP_EQUAL(wt_1, eve::pio_4(eve::as(wt_1)), 0.5);
 
     auto [wr_2, wt_2] = kyosu::to_polar(wo, o);
-    TTS_EQUAL(wr_2, eve::sqrt_2(eve::as(wr_2)));
-    TTS_EQUAL(wt_2, eve::pio_4(eve::as(wt_2)));
+    TTS_ULP_EQUAL(wr_2, eve::sqrt_2(eve::as(wr_2)), 0.5);
+    TTS_ULP_EQUAL(wt_2, eve::pio_4(eve::as(wt_2)), 0.5);
 
     auto [wr_3, wt_3] = kyosu::to_polar(o, wo);
-    TTS_EQUAL(wr_3, eve::sqrt_2(eve::as(wr_3)));
-    TTS_EQUAL(wt_3, eve::pio_4(eve::as(wt_3)));
+    TTS_ULP_EQUAL(wr_3, eve::sqrt_2(eve::as(wr_3)), 0.5);
+    TTS_ULP_EQUAL(wt_3, eve::pio_4(eve::as(wt_3)), 0.5);
   }
 };
--- test/unit/complex/to_polarpi.cpp
+++ test/unit/complex/to_polarpi.cpp
@@ -30,24 +30,24 @@ TTS_CASE_TPL("Check polarpi", kyosu::scalar_real_types)
   {
     auto [r_1, t_1] = kyosu::to_polarpi(o, o);
 
-    TTS_EQUAL(r_1, eve::sqrt_2(eve::as(r_1)));
-    TTS_EQUAL(t_1, eve::quarter(eve::as(t_1)));
+    TTS_ULP_EQUAL(r_1, eve::sqrt_2(eve::as(r_1)), 0.5);
+    TTS_ULP_EQUAL(t_1, eve::quarter(eve::as(t_1)), 0.5);
 
     auto [rc, tc] = kyosu::to_polarpi(c_t(1, 1));
 
-    TTS_EQUAL(rc, eve::sqrt_2(eve::as(rc)));
-    TTS_EQUAL(tc, eve::quarter(eve::as(tc)));
+    TTS_ULP_EQUAL(rc, eve::sqrt_2(eve::as(rc)), 0.5);
+    TTS_ULP_EQUAL(tc, eve::quarter(eve::as(tc)), 0.5);
 
     auto [wr_1, wt_1] = kyosu::to_polarpi(wo, wo);
-    TTS_EQUAL(wr_1, eve::sqrt_2(eve::as(wr_1)));
-    TTS_EQUAL(wt_1, eve::quarter(eve::as(wt_1)));
+    TTS_ULP_EQUAL(wr_1, eve::sqrt_2(eve::as(wr_1)), 0.5);
+    TTS_ULP_EQUAL(wt_1, eve::quarter(eve::as(wt_1)), 0.5);
 
     auto [wr_2, wt_2] = kyosu::to_polarpi(wo, o);
-    TTS_EQUAL(wr_2, eve::sqrt_2(eve::as(wr_2)));
-    TTS_EQUAL(wt_2, eve::quarter(eve::as(wt_2)));
+    TTS_ULP_EQUAL(wr_2, eve::sqrt_2(eve::as(wr_2)), 0.5);
+    TTS_ULP_EQUAL(wt_2, eve::quarter(eve::as(wt_2)), 0.5);
 
     auto [wr_3, wt_3] = kyosu::to_polarpi(o, wo);
-    TTS_EQUAL(wr_3, eve::sqrt_2(eve::as(wr_3)));
-    TTS_EQUAL(wt_3, eve::quarter(eve::as(wt_3)));
+    TTS_ULP_EQUAL(wr_3, eve::sqrt_2(eve::as(wr_3)), 0.5);
+    TTS_ULP_EQUAL(wt_3, eve::quarter(eve::as(wt_3)), 0.5);
   }
 };
