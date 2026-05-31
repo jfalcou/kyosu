6d209cf9637c5b4839d54f7835ab67bdb2291afa
jtlap <jtlapreste@gmail.com>
2026-05-31 09:13:50 +0200

    to_polarpi.cpp

--- test/unit/function/to_polarpi.cpp
+++ test/unit/function/to_polarpi.cpp
@@ -13,25 +13,25 @@ TTS_CASE_WITH("Check behavior of to_polarpi on wide",
               tts::randoms(-1.0, +1.0),
               tts::randoms(-1.0, +1.0)
 
-)
-<typename T>(T const& a0, T const& a1){
-
-auto c = kyosu::complex(a0, a1);
-auto [r1, t1] = kyosu::to_polarpi(c);
-auto c1 = kyosu::from_polarpi(r1, t1);
-TTS_RELATIVE_EQUAL(c, c1, tts::prec<T>());
-
-auto a = kyosu::complex(T(1), T(1));
-auto [ra, ta] = kyosu::to_polarpi(a);
-auto r =  kyosu::abs(a);
-auto t =  kyosu::arg[eve::radpi](a);
-TTS_ULP_EQUAL(r, ra, 0.5);
-TTS_ULP_EQUAL(t, ta, 0.5);
-}
+             )
+  <typename T>(T const& a0, T const& a1)
 {
-  auto [r1, t1] = kyosu::to_polarpi(a0);
-  auto c1 = kyosu::from_polarpi(r1, t1);
-  TTS_RELATIVE_EQUAL(kyosu::complex(a0), c1, tts::prec<T>());
-}
-}
-;
+  {
+    auto c = kyosu::complex(a0, a1);
+    auto [r1, t1] = kyosu::to_polarpi(c);
+    auto c1 = kyosu::from_polarpi(r1, t1);
+    TTS_RELATIVE_EQUAL(c, c1, tts::prec<T>());
+
+    auto a = kyosu::complex(T(1), T(1));
+    auto [ra, ta] = kyosu::to_polarpi(a);
+    auto r =  kyosu::abs(a);
+    auto t =  kyosu::arg[eve::radpi](a);
+    TTS_ULP_EQUAL(r, ra, 0.5);
+    TTS_ULP_EQUAL(t, ta, 0.5);
+  }
+  {
+    auto [r1, t1] = kyosu::to_polarpi(a0);
+    auto c1 = kyosu::from_polarpi(r1, t1);
+    TTS_RELATIVE_EQUAL(kyosu::complex(a0), c1, tts::prec<T>());
+  }
+};
