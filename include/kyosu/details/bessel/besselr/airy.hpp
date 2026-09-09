31721c9b5f7d2f7182e9266764dab9e16955e678
jtlap <jtlapreste@gmail.com>
2026-09-09 18:22:47 +0200

    unused things

--- include/kyosu/details/bessel/besselr/airy.hpp
+++ include/kyosu/details/bessel/besselr/airy.hpp
@@ -87,7 +87,7 @@ namespace kyosu::_
     auto invpi = eve::inv_pi(as<u_t>());
     auto br_im_eq_0 = [](auto z) { return Z(eve::airy_bi(real(z))); };
 
-    auto br_re_lt_0 = [third, invpi](auto z) {
+    auto br_re_lt_0 = [third](auto z) {
       auto zet = 2 * pow(-z, u_t(1.5)) / 3;
       return (cb_jr(-third, zet) - cb_jr(third, zet)) * sqrt(-z / 3);
     };
